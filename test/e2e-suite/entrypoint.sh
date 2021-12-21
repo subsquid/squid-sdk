f075ffb9e184bcec0c71926f849ad11b3ea8849c
Eldar Gabdullin <eldargab@gmail.com>
2021-12-21 23:13:22 +0300

    fix e2e setup

--- test/e2e-suite/entrypoint.sh
+++ test/e2e-suite/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./node_modules/.bin/mocha --delay --exit -r lib/setup.js --timeout 80 lib/*.test.js
+./node_modules/.bin/mocha --delay --exit -r lib/setup.js --timeout 80000 lib/*.test.js
