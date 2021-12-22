7918bee99c6cfd893900cb54cfd72a51a7f38886
Eldar Gabdullin <eldargab@gmail.com>
2021-12-22 22:00:41 +0300

    remove scaffold

--- cli/resource/scaffold/src/mappings/index.ts
+++
@@ -1,8 +0,0 @@
-import {SubstrateProcessor} from "@subsquid/substrate-processor"
-
-const processor = new SubstrateProcessor('kusama')
-
-// Archive GraphQL endpoint to fetch data
-processor.setIndexer('https://kusama.indexer.gc.subsquid.io/v4/graphql')
-
-processor.run()
