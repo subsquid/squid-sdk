a0c1131148491c9dd9a97b95e9de34372d7ae2b7
Eldar Gabdullin <eldargab@gmail.com>
2022-02-23 13:01:25 +0300

    fix not enough columns in result set regression when typeorm connection is used

--- graphql-server/src/test/resolvers.test.ts
+++ graphql-server/src/test/resolvers.test.ts
@@ -4,7 +4,11 @@ import {useServer} from "./util/server"
 describe('resolvers extension', function () {
     useDatabase([
         `create table scalar (id text primary key, "bool" bool, date timestamptz, big_number numeric, "bytes" bytea)`,
-        `insert into scalar (id, bool, date, big_number, "bytes") values ('1', true, '2021-09-24T00:00:00.000Z', 1000000000000000, decode('aa', 'hex'))`
+        `insert into scalar (id, bool, date, big_number, "bytes") values ('1', true, '2021-09-24T00:00:00.000Z', 1000000000000000, decode('aa', 'hex'))`,
+        `create table parent (id text primary key , name text)`,
+        `create table child (id text primary key , name text, parent_id text references parent)`,
+        `insert into parent (id, name) values ('1', 'hello')`,
+        `insert into child (id, name, parent_id) values ('2', 'world', '1')`
     ])
 
     const client = useServer('lib/test/resolvers-extension')
@@ -50,6 +54,28 @@ describe('resolvers extension', function () {
                 bigNumber: '1000000000000000',
                 bytes: '0xaa'
             }]
+        })
+    })
+
+    it('not enough columns in result regression', function() {
+        return client.test(`
+            query {
+                children {
+                    name
+                    parent {
+                        name
+                    }
+                }
+            }
+        `, {
+            children: [
+                {
+                    parent: {
+                        name: 'hello'
+                    },
+                    name: 'world'
+                }
+            ]
         })
     })
 })
--- openreader/src/queryBuilder.ts
+++ openreader/src/queryBuilder.ts
@@ -1,6 +1,5 @@
 import {toSnakeCase} from "@subsquid/util"
 import assert from "assert"
-import type {ClientBase, QueryArrayResult} from "pg"
 import {Database} from "./db"
 import type {Entity, JsonObject, Model} from "./model"
 import {getEntity, getFtsQuery, getObject, getUnionProps} from "./model.tools"
@@ -81,7 +80,7 @@ export class QueryBuilder {
                 break
             default:
                 if (columns.size()) {
-                    out += `SELECT ${columns.render()}\n`
+                    out += `SELECT ${columns.render(true)}\n`
                 }
         }
 
@@ -724,8 +723,12 @@ class ColumnSet {
         return idx
     }
 
-    render(): string {
-        return Array.from(this.columns.keys()).join(', ')
+    render(withAliases?: boolean): string {
+        let cols = Array.from(this.columns.keys())
+        if (withAliases) {
+            cols = cols.map((col, idx) => `${col} AS _c${idx}`)
+        }
+        return cols.join(', ')
     }
 
     size(): number {
