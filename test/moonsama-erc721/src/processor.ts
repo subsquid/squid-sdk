7b1b0ec97f776b1a9e38b667f0c2b7491c98983a
Eldar Gabdullin <eldargab@gmail.com>
2022-02-24 12:27:46 +0300

    first evm event decoded

--- test/moonsama-erc721/src/processor.ts
+++ test/moonsama-erc721/src/processor.ts
@@ -1,4 +1,5 @@
 import {SubstrateEvmProcessor} from "@subsquid/substrate-evm-processor"
+import * as erc721 from "./erc721"
 
 
 const processor = new SubstrateEvmProcessor('erc721')
@@ -9,6 +10,7 @@ processor.setTypesBundle('moonriver')
 processor.setDataSource({
     chain: 'wss://moonriver.api.onfinality.io/public-ws',
     archive: 'http://localhost:8080/v1/graphql'
+    // archive: 'https://moonriver-beta.indexer.gc.subsquid.io/v4/graphql'
 })
 
 
@@ -16,15 +18,13 @@ processor.setBlockRange({from: 568970})
 
 
 processor.addEvmLogHandler(
-    '0x63f2adf5f76f00d48fe2cbef19000af13bb8de82',
+    '0xb654611f84a8dc429ba3cb4fda9fad236c505a1a',
     {
-        topics: ['0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef']
+        topics: [erc721.events['Transfer(address,address,uint256)'].topic]
     },
     async ctx => {
-        console.log('event :', ctx.substrate.event.id)
-        console.log('contract: ', ctx.contractAddress)
-        console.log('topics:', ctx.topics)
-        console.log('data  :', ctx.data)
+        let transfer = erc721.events['Transfer(address,address,uint256)'].decode(ctx)
+        console.log(transfer)
     }
 )
 
