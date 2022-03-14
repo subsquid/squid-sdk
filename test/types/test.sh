e2407135e20e234295f008eb9c7ce1a581ededd8
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-03-15 01:09:26 +0500

    fix bifrost types bundle

--- test/types/test.sh
+++ test/types/test.sh
@@ -2,7 +2,7 @@
 
 rm -rf src || exit 1
 
-for chain in polkadot kusama khala altair astar shiden crust statemine statemint subsocial hydradx; do
+for chain in bifrost; do
   echo -n "gen $chain: "
   npx squid-substrate-typegen "config/$chain.json" || exit 1
   echo ok
