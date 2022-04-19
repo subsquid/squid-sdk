3fde831320476c23186e30476b2a96037433f5f0
Eldar Gabdullin <eldargab@gmail.com>
2022-04-19 17:22:57 +0300

    inline docs for SubstrateProcessor

--- substrate-processor/src/processor.ts
+++ substrate-processor/src/processor.ts
@@ -7,7 +7,14 @@ import {ChainManager} from "./chain"
 import {Db, IsolationLevel} from "./db"
 import {DataBatch, Ingest} from "./ingest"
 import {EvmLogEvent, EvmLogHandler, EvmTopicSet} from "./interfaces/evm"
-import {BlockHandler, BlockHandlerContext, EventHandler, ExtrinsicHandler} from "./interfaces/handlerContext"
+import type {
+    BlockHandler,
+    BlockHandlerContext,
+    EventHandler,
+    EventHandlerContext,
+    ExtrinsicHandler,
+    ExtrinsicHandlerContext
+} from "./interfaces/handlerContext"
 import {Hooks} from "./interfaces/hooks"
 import {QualifiedName, SubstrateEvent} from "./interfaces/substrate"
 import {ProgressTracker} from "./progress-tracker"
@@ -18,17 +25,31 @@ import {ServiceManager} from "./util/sm"
 
 
 export interface BlockHookOptions {
+    /**
+     * Inclusive range of blocks for which data handler is effective
+     */
     range?: Range
 }
 
 
 export interface EventHandlerOptions {
+    /**
+     * Inclusive range of blocks for which data handler is effective
+     */
     range?: Range
 }
 
 
 export interface ExtrinsicHandlerOptions {
+    /**
+     * Inclusive range of blocks for which data handler is effective
+     */
     range?: Range
+    /**
+     * A set of trigger events.
+     *
+     * See {@link SubstrateProcessor.addExtrinsicHandler} for more details.
+     */
     triggerEvents?: QualifiedName[]
 }
 
@@ -45,6 +66,9 @@ export interface DataSource {
 }
 
 
+/**
+ * Provides methods to configure and launch data processing.
+ */
 export class SubstrateProcessor {
     protected hooks: Hooks = {pre: [], post: [], event: [], extrinsic: [], evmLog: []}
     private blockRange: Range = {from: 0}
@@ -55,13 +79,53 @@ export class SubstrateProcessor {
     private isolationLevel?: IsolationLevel
     private running = false
 
+    /**
+     * @param name Defines prefix for a name of database schema
+     * under which the processor will keep its state.
+     */
     constructor(private name: string) {}
 
+    /**
+     * Sets blockchain data source.
+     *
+     * Currently, requires both chain node RPC WS endpoint and archive gateway.
+     *
+     * @example
+     * processor.setDataSource({
+     *     chain: 'wss://rpc.polkadot.io',
+     *     archive: 'https://polkadot.indexer.gc.subsquid.io/v4/graphql'
+     * })
+     */
     setDataSource(src: DataSource): void {
         this.assertNotRunning()
         this.src = src
     }
 
+    /**
+     * Sets types bundle.
+     *
+     * Types bundle is only required for blocks which have
+     * metadata version below 14.
+     *
+     * Don't confuse this setting with types bundle from polkadot.js.
+     * Although those two are similar in purpose and structure,
+     * they are not compatible.
+     *
+     * Types bundle can be specified in 3 different ways:
+     *
+     * 1. as a name of a known chain
+     * 2. as a name of a JSON file structured as {@link OldTypesBundle}
+     * 3. as an {@link OldTypesBundle} object
+     *
+     * @example
+     * processor.setTypesBundle('kusama') // known chain
+     * processor.setTypesBundle('typesBundle.json') // JSON file
+     * processor.setTypesBundle({ // OldTypesBundle object
+     *     types: {
+     *         Foo: 'u8'
+     *     }
+     * })
+     */
     setTypesBundle(bundle: string | OldTypesBundle): void {
         this.assertNotRunning()
         if (typeof bundle == 'string') {
@@ -71,22 +135,62 @@ export class SubstrateProcessor {
         }
     }
 
+    /**
+     * Limits the range of blocks to be processed.
+     *
+     * When upper bound is specified,
+     * the processor will terminate with exit code 0 once it reaches it.
+     *
+     * @example
+     * // process only block 100
+     * processor.setBlockRange({
+     *     from: 100,
+     *     to: 100
+     * })
+     */
     setBlockRange(range: Range): void {
         this.assertNotRunning()
         this.blockRange = range
     }
 
+    /**
+     * Sets the maximum number of blocks which can be fetched
+     * from the data source in one request.
+     *
+     * The default is 100.
+     *
+     * Usually this setting doesn't have significant impact on performance.
+     */
     setBatchSize(size: number): void {
         this.assertNotRunning()
         assert(size > 0)
         this.batchSize = size
     }
 
+    /**
+     * Sets the port for built-in prometheus metrics server.
+     *
+     * By default, the value of `PROMETHEUS_PORT` environment
+     * variable is used. When it is not set,
+     * the processor will pick up an ephemeral port.
+     */
     setPrometheusPort(port: number | string) {
         this.assertNotRunning()
         this.prometheusPort = port
     }
 
+    /**
+     * Sets an isolation level for database transactions
+     * in which data handlers are executed.
+     *
+     * Defaults to `SERIALIZABLE`.
+     *
+     * This setting is for complex scenarios when
+     * there are another database writers beside the processor.
+     *
+     * Note, that altering this setting can easily lead to "hard to debug"
+     * consistency issues.
+     */
     setIsolationLevel(isolationLevel?: IsolationLevel): void {
         this.assertNotRunning()
         this.isolationLevel = isolationLevel
@@ -98,6 +202,26 @@ export class SubstrateProcessor {
             : this.prometheusPort
     }
 
+    /**
+     * Registers a block level data handler which will be executed before
+     * any further processing.
+     *
+     * See {@link BlockHandlerContext} for an API available to the handler.
+     *
+     * Block level handlers affect performance, as they are
+     * triggered for all chain blocks. Otherwise,
+     * the processor skips blocks without interesting data.
+     *
+     * Relative execution order for multiple pre-block hooks is currently not defined.
+     *
+     * @example
+     * processor.addPreHook(async ctx => {
+     *     console.log(ctx.block.height)
+     * })
+     *
+     * // limit the range of blocks for which pre-block hook will be effective
+     * processor.addPreHook({range: {from: 100000}}, async ctx => {})
+     */
     addPreHook(fn: BlockHandler): void
     addPreHook(options: BlockHookOptions, fn: BlockHandler): void
     addPreHook(fnOrOptions: BlockHandler | BlockHookOptions, fn?: BlockHandler): void {
@@ -113,6 +237,26 @@ export class SubstrateProcessor {
         this.hooks.pre.push({handler, ...options})
     }
 
+    /**
+     * Registers a block level data handler which will be executed
+     * at the end of processing.
+     *
+     * See {@link BlockHandlerContext} for an API available to the handler.
+     *
+     * Block level handlers affect performance, as they are
+     * triggered for all chain blocks.
+     * Otherwise, the processor skips blocks without interesting data.
+     *
+     * Relative execution order for multiple post-block hooks is currently not defined.
+     *
+     * @example
+     * processor.addPostHook(async ctx => {
+     *     console.log(ctx.block.height)
+     * })
+     *
+     * // limit the range of blocks for which post-block hook will be effective
+     * processor.addPostHook({range: {from: 100000}}, async ctx => {})
+     */
     addPostHook(fn: BlockHandler): void
     addPostHook(options: BlockHookOptions, fn: BlockHandler): void
     addPostHook(fnOrOptions: BlockHandler | BlockHookOptions, fn?: BlockHandler): void {
@@ -128,6 +272,28 @@ export class SubstrateProcessor {
         this.hooks.post.push({handler, ...options})
     }
 
+    /**
+     * Registers event data handler.
+     *
+     * See {@link EventHandlerContext} for an API available to the handler.
+     *
+     * All events are processed sequentially according to event log of the current block.
+     *
+     * Relative execution order is currently not defined for multiple event handlers
+     * registered for the same event.
+     *
+     * @example
+     * processor.addEventHandler('balances.Transfer', async ctx => {
+     *     assert(ctx.event.name == 'balances.Transfer')
+     * })
+     *
+     * // limit the range of blocks for which event handler will be effective
+     * processor.addEventHandler('balances.Transfer', {
+     *     range: {from: 100000}
+     * }, async ctx => {
+     *     assert(ctx.event.name == 'balances.Transfer')
+     * })
+     */
     addEventHandler(eventName: QualifiedName, fn: EventHandler): void
     addEventHandler(eventName: QualifiedName, options: EventHandlerOptions, fn: EventHandler): void
     addEventHandler(eventName: QualifiedName, fnOrOptions: EventHandlerOptions | EventHandler, fn?: EventHandler): void {
@@ -147,6 +313,46 @@ export class SubstrateProcessor {
         })
     }
 
+    /**
+     * Registers extrinsic data handler.
+     *
+     * See {@link ExtrinsicHandlerContext} for an API available to the handler.
+     *
+     * Extrinsics are processed sequentially according
+     * to position of a `trigger event` in the event log of the current block.
+     * This not only defines relative order between extrinsics,
+     * but also between extrinsic and its events.
+     *
+     * The set of possible trigger events is customizable and defaults to `['system.ExtrinsicSuccess']`.
+     * Which means, by default extrinsic handler will be executed after all events of the same extrinsic
+     * and only if it was successful.
+     *
+     * Relative execution order is currently not defined for multiple extrinsic handlers
+     * registered for the same extrinsic and trigger events.
+     *
+     * @example
+     * processor.addExtrinsicHandler('balances.transfer', async ctx => {
+     *     assert(ctx.extrinsic.name == 'balances.transfer')
+     * })
+     *
+     * // limit the range of blocks for which extrinsic handler will be effective
+     * processor.addExtrinsicHandler('balances.transfer', {
+     *     range: {from: 100000}
+     * }, async ctx => {
+     *     assert(ctx.extrinsic.name == 'balances.transfer')
+     * })
+     *
+     * // handle both successful and failed extrinsics
+     * processor.addExtrinsicHandler('balances.transfer', {
+     *     triggerEvents: ['system.ExtrinsicSuccess', 'system.ExtrinsicFailed']
+     * }, async ctx => {
+     *     if (ctx.event.name == 'system.ExtrinsicSuccess') {
+     *         console.log('successful')
+     *     } else {
+     *         console.log('failed')
+     *     }
+     * })
+     */
     addExtrinsicHandler(extrinsicName: QualifiedName, fn: ExtrinsicHandler): void
     addExtrinsicHandler(extrinsicName: QualifiedName, options: ExtrinsicHandlerOptions, fn: ExtrinsicHandler): void
     addExtrinsicHandler(extrinsicName: QualifiedName, fnOrOptions: ExtrinsicHandler | ExtrinsicHandlerOptions, fn?: ExtrinsicHandler): void {
@@ -176,6 +382,13 @@ export class SubstrateProcessor {
         }
     }
 
+    /**
+     * Starts data processing.
+     *
+     * This method assumes full control over the current OS process as
+     * it terminates the entire program in case of error or
+     * at the end of data processing.
+     */
     run(): void {
         if (this.running) return
         this.running = true
