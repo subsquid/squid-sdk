77b4ce28a5b260d3a38460fb200b1bb0a6e6003a
Alexey Safonov <safonov.a.r@mail.ru>
2022-05-26 12:03:44 +0300

    add indexes for fields in archive (#44)

--- substrate-ingest/migrations/001-initial.sql
+++ substrate-ingest/migrations/001-initial.sql
@@ -23,6 +23,7 @@ CREATE TABLE block (
 CREATE INDEX IDX_block__height ON block(height);
 CREATE INDEX IDX_block__hash ON block(hash);
 CREATE INDEX IDX_block__timestamp ON block(timestamp);
+CREATE INDEX IDX_block__validator ON block(validator);
 
 
 CREATE TABLE extrinsic (
@@ -42,6 +43,9 @@ CREATE TABLE extrinsic (
 
 
 CREATE INDEX IDX_extrinsic__block__index ON extrinsic(block_id, index_in_block);
+CREATE INDEX IDX_extrinsic__hash ON extrinsic(hash);
+CREATE INDEX IDX_extrinsic__call ON extrinsic(call_id);
+CREATE INDEX IDX_extrinsic__signature ON extrinsic USING GIN (signature);
 
 
 CREATE TABLE call (
@@ -61,6 +65,7 @@ CREATE TABLE call (
 CREATE INDEX IDX_call__name__block ON call(name, block_id);
 CREATE INDEX IDX_call__extrinsic__index ON call(extrinsic_id);
 CREATE INDEX IDX_call__parent ON call(parent_id);
+CREATE INDEX IDX_call__block ON call(block_id);
 
 
 CREATE TABLE event (
