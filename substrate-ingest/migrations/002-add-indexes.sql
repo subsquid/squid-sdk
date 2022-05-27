07474237158c76f1584805d78832d5367be89831
Eldar Gabdullin <eldargab@gmail.com>
2022-05-27 14:58:31 +0300

    fix migrations

--- substrate-ingest/migrations/002-add-indexes.sql
+++ substrate-ingest/migrations/002-add-indexes.sql
@@ -5,5 +5,3 @@ CREATE INDEX IDX_extrinsic__call ON extrinsic(call_id);
 CREATE INDEX IDX_extrinsic__signature ON extrinsic USING GIN (signature);
 
 CREATE INDEX IDX_call__block ON call(block_id);
-
-CREATE INDEX IDX_event__args ON event USING GIN (args);
