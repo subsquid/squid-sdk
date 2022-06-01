3404eb5c9d48954f14c55509e3bf0d39decb37e1
Eldar Gabdullin <eldargab@gmail.com>
2022-06-02 00:13:54 +0300

    add insert op

--- stores/typeorm-store/src/store.ts
+++ stores/typeorm-store/src/store.ts
@@ -1,4 +1,9 @@
-import type {EntityManager, EntityTarget, FindConditions, FindManyOptions, FindOneOptions} from "typeorm"
+import type {EntityManager, FindConditions, FindManyOptions, FindOneOptions} from "typeorm"
+
+
+export interface EntityClass<T> {
+    new (): T
+}
 
 
 export class Store {
@@ -10,8 +15,12 @@ export class Store {
 
     save<Entity>(entity: Entity): Promise<Entity>
     save<Entity>(entities: Entity[]): Promise<Entity[]>
-    save<Entity>(e: Entity | Entity[]): Promise<Entity | Entity[]>{
+    save<Entity>(e: Entity | Entity[]): Promise<Entity | Entity[]> {
         return this.em().then(em => em.save(e))
+    }
+
+    insert<Entity extends object>(e: Entity): Promise<void> {
+        return this.em().then(em => em.insert(e.constructor, e)).then()
     }
 
     remove<Entity>(entity: Entity): Promise<Entity>
@@ -20,44 +29,44 @@ export class Store {
         return this.em().then(em => em.remove(e))
     }
 
-    count<Entity>(entityClass: EntityTarget<Entity>, options?: FindOneOptions<Entity>): Promise<number>
-    count<Entity>(entityClass: EntityTarget<Entity>, options?: FindManyOptions<Entity>): Promise<number>
-    count<Entity>(entityClass: EntityTarget<Entity>, conditions?: FindConditions<Entity>): Promise<number>
+    count<Entity>(entityClass: EntityClass<Entity>, options?: FindOneOptions<Entity>): Promise<number>
+    count<Entity>(entityClass: EntityClass<Entity>, options?: FindManyOptions<Entity>): Promise<number>
+    count<Entity>(entityClass: EntityClass<Entity>, conditions?: FindConditions<Entity>): Promise<number>
     count(entityClass: any, options?: any): Promise<number> {
         return this.em().then(em => em.count(entityClass, options))
     }
 
-    find<Entity>(entityClass: EntityTarget<Entity>, options?: FindManyOptions<Entity>): Promise<Entity[]>
-    find<Entity>(entityClass: EntityTarget<Entity>, conditions?: FindConditions<Entity>): Promise<Entity[]>
+    find<Entity>(entityClass: EntityClass<Entity>, options?: FindManyOptions<Entity>): Promise<Entity[]>
+    find<Entity>(entityClass: EntityClass<Entity>, conditions?: FindConditions<Entity>): Promise<Entity[]>
     find(entityClass: any, options?: any): Promise<any[]> {
         return this.em().then(em => em.find(entityClass, options))
     }
 
-    findByIds<Entity>(entityClass: EntityTarget<Entity>, ids: string[], options?: FindManyOptions<Entity>): Promise<Entity[]>
-    findByIds<Entity>(entityClass: EntityTarget<Entity>, ids: string[], conditions?: FindConditions<Entity>): Promise<Entity[]>
+    findByIds<Entity>(entityClass: EntityClass<Entity>, ids: string[], options?: FindManyOptions<Entity>): Promise<Entity[]>
+    findByIds<Entity>(entityClass: EntityClass<Entity>, ids: string[], conditions?: FindConditions<Entity>): Promise<Entity[]>
     findByIds(entityClass: any, ids: any[], options?: any): Promise<any[]> {
         return this.em().then(em => em.findByIds(entityClass, ids, options))
     }
 
-    findOne<Entity>(entityClass: EntityTarget<Entity>, id?: string, options?: FindOneOptions<Entity>): Promise<Entity | undefined>
-    findOne<Entity>(entityClass: EntityTarget<Entity>, options?: FindOneOptions<Entity>): Promise<Entity | undefined>
-    findOne<Entity>(entityClass: EntityTarget<Entity>, conditions?: FindConditions<Entity>, options?: FindOneOptions<Entity>): Promise<Entity | undefined>
+    findOne<Entity>(entityClass: EntityClass<Entity>, id?: string, options?: FindOneOptions<Entity>): Promise<Entity | undefined>
+    findOne<Entity>(entityClass: EntityClass<Entity>, options?: FindOneOptions<Entity>): Promise<Entity | undefined>
+    findOne<Entity>(entityClass: EntityClass<Entity>, conditions?: FindConditions<Entity>, options?: FindOneOptions<Entity>): Promise<Entity | undefined>
     findOne(entityClass: any, conditions?: any, options?: any): Promise<any | undefined> {
         return this.em().then(em => em.findOne(entityClass, conditions, options))
     }
 
-    findOneOrFail<Entity>(entityClass: EntityTarget<Entity>, id?: string, options?: FindOneOptions<Entity>): Promise<Entity>
-    findOneOrFail<Entity>(entityClass: EntityTarget<Entity>, options?: FindOneOptions<Entity>): Promise<Entity>
-    findOneOrFail<Entity>(entityClass: EntityTarget<Entity>, conditions?: FindConditions<Entity>, options?: FindOneOptions<Entity>): Promise<Entity>
+    findOneOrFail<Entity>(entityClass: EntityClass<Entity>, id?: string, options?: FindOneOptions<Entity>): Promise<Entity>
+    findOneOrFail<Entity>(entityClass: EntityClass<Entity>, options?: FindOneOptions<Entity>): Promise<Entity>
+    findOneOrFail<Entity>(entityClass: EntityClass<Entity>, conditions?: FindConditions<Entity>, options?: FindOneOptions<Entity>): Promise<Entity>
     findOneOrFail(entityClass: any, conditions?: any, options?: any): Promise<any | undefined> {
         return this.em().then(em => em.findOneOrFail(entityClass, conditions, options))
     }
 
-    clear<Entity>(entityClass: EntityTarget<Entity>): Promise<void> {
+    clear<Entity>(entityClass: EntityClass<Entity>): Promise<void> {
         return this.em().then(em => em.clear(entityClass))
     }
 
-    get<Entity>(entityClass: EntityTarget<Entity>, optionsOrId?: FindOneOptions<Entity> | string): Promise<Entity | undefined> {
+    get<Entity>(entityClass: EntityClass<Entity>, optionsOrId?: FindOneOptions<Entity> | string): Promise<Entity | undefined> {
         if (typeof optionsOrId == 'string') { // please compiler
             return this.findOne(entityClass, optionsOrId)
         } else {
