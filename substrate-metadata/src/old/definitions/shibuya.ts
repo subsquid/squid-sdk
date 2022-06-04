81a6e18ac075323f48afcfe749b2e42544cc6765
tmcgroul <safonov.a.r@mail.ru>
2022-06-04 16:29:32 +0300

    add shibuya typesbundle

---
+++ substrate-metadata/src/old/definitions/shibuya.ts
@@ -0,0 +1,44 @@
+import { OldTypesBundle } from "../types"
+
+export const bundle: OldTypesBundle = {
+  types: {},
+  versions: [
+    {
+      minmax: [0, null],
+      types: {
+        Keys: 'AccountId',
+        Address: 'MultiAddress',
+        LookupSource: 'MultiAddress',
+        AmountOf: 'Amount',
+        Amount: 'i128',
+        SmartContract: {
+          _enum: {
+            Evm: 'H160',
+            Wasm: 'AccountId'
+          }
+        },
+        EraStakingPoints: {
+          total: 'Balance',
+          stakers: 'BTreeMap<AccountId, Balance>',
+          formerStakedEra: 'EraIndex',
+          claimedRewards: 'Balance'
+        },
+        PalletDappsStakingEraStakingPoints: {
+          total: 'Balance',
+          stakers: 'BTreeMap<AccountId, Balance>',
+          formerStakedEra: 'EraIndex',
+          claimedRewards: 'Balance'
+        },
+        EraRewardAndStake: {
+          rewards: 'Balance',
+          staked: 'Balance'
+        },
+        PalletDappsStakingEraRewardAndStake: {
+          rewards: 'Balance',
+          staked: 'Balance'
+        },
+        EraIndex: 'u32'
+      }
+    }
+  ]
+}
