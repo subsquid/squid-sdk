fa4ba99ccb40fc5efec6ed83b358159a77666585
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-07-02 14:32:12 +0300

    eth-processor: fix runner problems

--- test/ethereum-name-service/src/processor.ts
+++ test/ethereum-name-service/src/processor.ts
@@ -7,12 +7,12 @@ require('dotenv').config()
 const processor = new Processor(new TypeormDatabase())
 
 processor.setDataSource({
-    archive: 'http://192.168.240.1:8080'
+    archive: 'http://172.30.144.1:8080'
 })
 
 processor.setBlockRange({
     from: 11000000,
-    to: 11050000,
+    to: 14150000,
 })
 
 processor.setBatchSize(5000)
@@ -21,40 +21,43 @@ processor.setFieldSelection({
     tx: {},
     block: {},
     data: true,
-    topic0: true,
-    topic1: true,
-    topic2: true,
-    topic3: true,
 });
 
 processor.addEvmLogHandler(
     "0x314159265dd8dbb310642f98f50c066173c1259b",
-    [],
+    [
+        [registry.events['Transfer(bytes32,address)'].topic]
+    ],
     async ctx => {
-        if (!ctx.log.data) {
-            console.log("no data");
-            return;
+        if(!ctx.log.data) {
+            throw new Error("no data");
         }
 
-        try {
-            const transfer = registry.events['Transfer(bytes32,address)'].decode({
-                data: ctx.log.data,
-                topics: ctx.log.topics,
-            })
-    
-            console.log(ctx.log.topics[0] === registry.events['Transfer(bytes32,address)'].topic)
-            console.log(transfer)
-        } catch {}
+        const transfer = registry.events['Transfer(bytes32,address)'].decode({
+            data: ctx.log.data,
+            topics: ctx.log.topics,
+        });
 
-        try {
-            const transfer = registry.events["NewTTL(bytes32,uint64)"].decode({
-                data: ctx.log.data,
-                topics: ctx.log.topics,
-            })
-    
-            console.log(ctx.log.topics[0] === registry.events["NewTTL(bytes32,uint64)"].topic)
-            console.log(transfer)
-        } catch {}
+        console.log(`TRANSFER: ${JSON.stringify(transfer, null, 2)}`);
+    }
+)
+
+processor.addEvmLogHandler(
+    "0x314159265dd8dbb310642f98f50c066173c1259b",
+    [
+        [registry.events["NewTTL(bytes32,uint64)"].topic]
+    ],
+    async ctx => {
+        if(!ctx.log.data) {
+            throw new Error("no data");
+        }
+
+        const newTtl = registry.events["NewTTL(bytes32,uint64)"].decode({
+            data: ctx.log.data,
+            topics: ctx.log.topics,
+        });
+
+        console.log(`NEW_TTL: ${JSON.stringify(newTtl, null, 2)}`);
     }
 )
 
