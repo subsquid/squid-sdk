49246981fdd41624a5dfed4b823e4407236cfccc
Nikita Klimin <nklimin007@gmail.com>
2022-07-28 14:10:53 +0300

    refactor: change routes

--- cli/src/api/secrets.ts
+++ cli/src/api/secrets.ts
@@ -4,7 +4,7 @@ import { api } from './api';
 export async function listSecrets(): Promise<SecretsListResponse> {
     const { body } = await api<HttpResponse<SecretsListResponse>>( {
         method: 'get',
-        path: '/client/secrets/list'
+        path: '/secrets/list'
     });
     return body.payload
 }
@@ -12,7 +12,7 @@ export async function listSecrets(): Promise<SecretsListResponse> {
 export async function removeSecret(name: string): Promise<SetSecretResponse> {
     const { body } = await api<HttpResponse<SetSecretResponse>>( {
         method: 'delete',
-        path: `/client/secrets/${name}`,
+        path: `/secrets/${name}`,
     });
     return body.payload
 }
@@ -20,7 +20,7 @@ export async function removeSecret(name: string): Promise<SetSecretResponse> {
 export async function setSecret(name: string, value: string): Promise<SetSecretResponse> {
     const { body } = await api<HttpResponse<SetSecretResponse>>( {
         method: 'put',
-        path: `/client/secrets/${name}`,
+        path: `/secrets/${name}`,
         data: { value }
     });
     return body.payload
