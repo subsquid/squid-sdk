2efbb10f7ecfc0a9f62a1b19d470b3dc999a7260
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-08-15 15:16:05 +0500

    refactor

--- evm-typegen/src/typegen.ts
+++ evm-typegen/src/typegen.ts
@@ -1,5 +1,6 @@
 import {Interface, ParamType} from "@ethersproject/abi"
 import {FileOutput, Output} from "@subsquid/util-internal-code-printer"
+import {def} from "@subsquid/util-internal"
 import fs from "fs"
 
 export class Typegen {
@@ -8,20 +9,20 @@ export class Typegen {
     private out: FileOutput
 
     constructor(abiFile: string, outPath: string) {
-        this.rawAbi = JSON.parse(fs.readFileSync(abiFile, {encoding: "utf-8"}));
+        this.rawAbi = JSON.parse(fs.readFileSync(abiFile, {encoding: "utf-8"}))
         this.abi = new Interface(this.rawAbi)
-        this.out = new FileOutput(outPath);
+        this.out = new FileOutput(outPath)
     }
 
     generate(): void {
-        this.out.line("import * as ethers from \"ethers\";");
-        this.out.line("import assert from \"assert\";");
+        this.out.line("import * as ethers from \"ethers\";")
+        this.out.line("import assert from \"assert\";")
         this.out.line()
-        this.out.line("export const abi = new ethers.utils.Interface(getJsonAbi());");
+        this.out.line("export const abi = new ethers.utils.Interface(getJsonAbi());")
         this.out.line()
-        this.generateDecoders('events')
+        this.generateEvents()
         this.out.line()
-        this.generateDecoders('functions')
+        this.generateFunctions()
         this.out.line()
         this.generateContract()
         this.out.line()
@@ -33,97 +34,110 @@ export class Typegen {
         this.out.write()
     }
 
-    private generateDecoders(kind: 'events' | 'functions') {
-        const declarations = kind === 'events' ? this.getEvents() : this.getFunctions()
-        const postfix = kind === 'events' ? 'Event' : 'Function'
-
-        for (const decl of declarations) {
+    private generateEvents() {
+        for (const decl of this.getEvents()) {
             for (let i = 0; i < decl.overloads.length; i++) {
                 if (decl.overloads[i].inputs.length === 0) continue
-                this.out.line(`export type ${decl.name}${i}${postfix} = ${getTupleType(decl.overloads[i].inputs)}`)
-                this.out.line("");
+                this.out.line(`export type ${decl.name}${i}Event = ${getTupleType(decl.overloads[i].inputs)}`)
+                this.out.line()
             }
         }
-        if (kind === 'events') {
-            this.out.block("export interface EvmLog", () => {
-                this.out.line("data: string;");
-                this.out.line("topics: string[];");
+        this.out.block("export interface EvmLog", () => {
+            this.out.line("data: string;")
+            this.out.line("topics: string[];")
+        })
+        this.out.line()
+        this.out.block(`function decodeEvent(signature: string, data: EvmLog): any`, () => {
+            this.out.line(`return abi.decodeEventLog(`)
+            this.out.indentation(() => {
+                this.out.line(`abi.getEvent(signature),`)
+                this.out.line(`data.data || "",`)
+                this.out.line("data.topics")
             })
-            this.out.line();
-            this.out.block(`function decodeEvent(signature: string, data: EvmLog): any`, () => {
-                this.out.line(`return abi.decodeEventLog(`);
-                this.out.indentation(() => {
-                    this.out.line(`abi.getEvent(signature),`);
-                    this.out.line(`data.data || "",`);
-                    this.out.line("data.topics");
-                });
-                this.out.line(");");
-            });
-        } else {
-            this.out.line();
-            this.out.block(`function decodeFunction(data: string): any`, () => {
-                this.out.line(`return abi.decodeFunctionData(data.slice(0, 10), data)`);
-            });
-        }
-        this.out.line();
-        this.out.block(`export const ${kind} =`, () => {
-            for (const decl of declarations) {
-                for (let i = 0; i < decl.overloads.length; i++) {
-                    const overload = decl.overloads[i]
-                    const signature = createSignature(decl.name, overload.inputs)
+            this.out.line(");")
+        })
+        this.out.line()
+        this.out.block(`export const events =`, () => {
+            for (const event of this.getEvents()) {
+                for (let i = 0; i < event.overloads.length; i++) {
+                    const overload = event.overloads[i]
+                    const signature = createSignature(event.name, overload.inputs)
                     this.out.block(`"${signature}":`, () => {
-                        if (kind === 'events') {
-                            this.out.line(`topic: abi.getEventTopic("${signature}"),`);
-                            if (decl.overloads[i].inputs.length > 0) {
-                                this.out.block(`decode(data: EvmLog): ${decl.name}${i}Event`, () => {
-                                    this.out.line(`return decodeEvent("${signature}", data)`)
-                                });
-                            }
-                        } else {
-                            this.out.line(`sighash: abi.getSighash("${signature}"),`);
-                            if (decl.overloads[i].inputs.length > 0)
-                                this.out.block(`decode(input: string): ${decl.name}${i}Function`, () => {
-                                    this.out.line(`return decodeFunction(input)`)
-                                });
+                        this.out.line(`topic: abi.getEventTopic("${signature}"),`)
+                        if (event.overloads[i].inputs.length > 0) {
+                            this.out.block(`decode(data: EvmLog): ${event.name}${i}Event`, () => {
+                                this.out.line(`return decodeEvent("${signature}", data)`)
+                            })
                         }
-                    });
+                    })
                     this.out.line(",")
                 }
             }
-        });
+        })
+    }
+
+    private generateFunctions() {
+        for (const func of this.getFunctions()) {
+            for (let i = 0; i < func.overloads.length; i++) {
+                if (func.overloads[i].inputs.length === 0) continue
+                this.out.line(`export type ${func.name}${i}Function = ${getTupleType(func.overloads[i].inputs)}`)
+                this.out.line()
+            }
+        }
+        this.out.line()
+        this.out.block(`function decodeFunction(data: string): any`, () => {
+            this.out.line(`return abi.decodeFunctionData(data.slice(0, 10), data)`)
+        })
+        this.out.line()
+        this.out.block(`export const functions =`, () => {
+            for (const func of this.getFunctions()) {
+                for (let i = 0; i < func.overloads.length; i++) {
+                    const overload = func.overloads[i]
+                    const signature = createSignature(func.name, overload.inputs)
+                    this.out.block(`"${signature}":`, () => {
+                        this.out.line(`sighash: abi.getSighash("${signature}"),`)
+                        if (func.overloads[i].inputs.length > 0)
+                            this.out.block(`decode(input: string): ${func.name}${i}Function`, () => {
+                                this.out.line(`return decodeFunction(input)`)
+                            })
+                    })
+                    this.out.line(",")
+                }
+            }
+        })
     }
 
     private generateContract() {
         let abiCalls = this.getCalls()
 
         this.out.block("interface ChainContext ", () => {
-            this.out.line(`_chain: Chain`);
+            this.out.line(`_chain: Chain`)
         })
-        this.out.line();
+        this.out.line()
         this.out.block("interface BlockContext ", () => {
-            this.out.line(`_chain: Chain`);
-            this.out.line(`block: Block`);
+            this.out.line(`_chain: Chain`)
+            this.out.line(`block: Block`)
         })
-        this.out.line();
+        this.out.line()
         this.out.block("interface Block ", () => {
-            this.out.line(`height: number`);
+            this.out.line(`height: number`)
         })
-        this.out.line();
+        this.out.line()
         this.out.block("interface Chain ", () => {
             this.out.block("client: ", () => {
-                this.out.line(`call: <T=any>(method: string, params?: unknown[]) => Promise<T>`);
+                this.out.line(`call: <T=any>(method: string, params?: unknown[]) => Promise<T>`)
             })
         })
-        this.out.line();
+        this.out.line()
         this.out.block("export class Contract ", () => {
-            this.out.line(`private readonly _chain: Chain`);
-            this.out.line(`private readonly blockHeight: number`);
-            this.out.line(`readonly address: string`);
-            this.out.line();
-            this.out.line(`constructor(ctx: BlockContext, address: string)`);
-            this.out.line(`constructor(ctx: ChainContext, block: Block, address: string)`);
+            this.out.line(`private readonly _chain: Chain`)
+            this.out.line(`private readonly blockHeight: number`)
+            this.out.line(`readonly address: string`)
+            this.out.line()
+            this.out.line(`constructor(ctx: BlockContext, address: string)`)
+            this.out.line(`constructor(ctx: ChainContext, block: Block, address: string)`)
             this.out.block(`constructor(ctx: BlockContext, blockOrAddress: Block | string, address?: string)`, () => {
-                this.out.line(`this._chain = ctx._chain`);
+                this.out.line(`this._chain = ctx._chain`)
                 this.out.block(`if (typeof blockOrAddress === 'string') `, () => {
                     this.out.line(`this.blockHeight = ctx.block.height`)
                     this.out.line(`this.address = ethers.utils.getAddress(blockOrAddress)`)
@@ -134,7 +148,7 @@ export class Typegen {
                     this.out.line(`this.address = ethers.utils.getAddress(address)`)
                 })
             })
-            this.out.line();
+            this.out.line()
             for (const decl of abiCalls) {
                 if (decl.overloads.length > 1) {
                     for (let overload of decl.overloads) {
@@ -143,28 +157,29 @@ export class Typegen {
                         this.out.line(`async ${decl.name}(${args}): Promise<${returnType}>`)
                     }
                     this.out.block(`async ${decl.name}(...args: any[])`, () => {
-                        this.out.line(`return this.call("${decl.name}", args)`);
-                    });
+                        this.out.line(`return this.call("${decl.name}", args)`)
+                    })
                 } else {
                     const overload = decl.overloads[0]
                     const params = overload.inputs.map((i, n) => `${i.name || `arg${n}`}: ${getType(i)}`)
                     const returnType = overload.outputs.length == 1 ? getType(overload.outputs[0]) : getTupleType(overload.outputs)
                     this.out.block(`async ${decl.name}(${params.join(`, `)}): Promise<${returnType}>`, () => {
-                        this.out.line(`return this.call("${decl.name}", [${overload.inputs.map((i, n) => `${i.name || `arg${n}`}`).join(`, `)}])`);
-                    });
+                        this.out.line(`return this.call("${decl.name}", [${overload.inputs.map((i, n) => `${i.name || `arg${n}`}`).join(`, `)}])`)
+                    })
                 }
-                this.out.line();
+                this.out.line()
             }
             this.out.block(`private async call(name: string, args: any[]) : Promise<any>`, () => {
-                this.out.line(`const fragment = abi.getFunction(name)`);
-                this.out.line(`const data = abi.encodeFunctionData(fragment, args)`);
-                this.out.line(`const result = await this._chain.client.call('eth_call', [{to: this.address, data}, this.blockHeight])`);
-                this.out.line(`const decoded = abi.decodeFunctionResult(fragment, result)`);
-                this.out.line(`return decoded.length > 1 ? decoded : decoded[0]`);
+                this.out.line(`const fragment = abi.getFunction(name)`)
+                this.out.line(`const data = abi.encodeFunctionData(fragment, args)`)
+                this.out.line(`const result = await this._chain.client.call('eth_call', [{to: this.address, data}, this.blockHeight])`)
+                this.out.line(`const decoded = abi.decodeFunctionResult(fragment, result)`)
+                this.out.line(`return decoded.length > 1 ? decoded : decoded[0]`)
             })
         })
     }
 
+    @def
     private getEvents(): AbiEvent[] {
         let res: Map<string, AbiEvent> = new Map()
         for (let event of Object.values(this.abi.events)) {
@@ -185,6 +200,7 @@ export class Typegen {
         return [...res.values()]
     }
 
+    @def
     private getFunctions(): AbiFunction[] {
         let res: Map<string, AbiFunction> = new Map()
         for (let func of Object.values(this.abi.functions)) {
@@ -207,6 +223,7 @@ export class Typegen {
         return [...res.values()]
     }
 
+    @def
     private getCalls(): AbiCall[] {
         let res: Map<string, AbiCall> = new Map()
         for (let func of Object.values(this.abi.functions)) {
@@ -252,14 +269,14 @@ function getType(param: ParamType): string {
     }
 
     if (param.baseType === "array") {
-        return "Array<" + getType(param.arrayChildren) + ">";
+        return "Array<" + getType(param.arrayChildren) + ">"
     }
 
     if (param.baseType === "tuple") {
-        return getTupleType(param.components);
+        return getTupleType(param.components)
     }
 
-    throw new Error("unknown type");
+    throw new Error("unknown type")
 }
 
 function getTupleType(params: ParamType[]) {
@@ -292,23 +309,23 @@ function createSignature(name: string, inputs: ParamType[]) {
 }
 
 interface AbiEvent {
-    name: string;
+    name: string
     overloads: {
-        inputs: ParamType[];
+        inputs: ParamType[]
     }[]
 }
 
 interface AbiFunction {
-    name: string;
+    name: string
     overloads: {
-        inputs: ParamType[];
+        inputs: ParamType[]
     }[]
 }
 
 interface AbiCall {
-    name: string;
+    name: string
     overloads: {
-        inputs: ParamType[];
-        outputs: ParamType[];
+        inputs: ParamType[]
+        outputs: ParamType[]
     }[]
 }
