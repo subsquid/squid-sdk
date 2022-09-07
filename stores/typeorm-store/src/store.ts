422953beb6636b6713617603c9d85ce40e4f1087
Eldar Gabdullin <eldargab@gmail.com>
2022-09-07 13:45:28 +0300

    fix: `Store.save()` can clear foreign keys on multi-row update

--- stores/typeorm-store/src/store.ts
+++ stores/typeorm-store/src/store.ts
@@ -1,6 +1,7 @@
 import assert from "assert"
-import type {EntityManager, FindOptionsOrder, FindOptionsRelations, FindOptionsWhere} from "typeorm"
+import {EntityManager, FindOptionsOrder, FindOptionsRelations, FindOptionsWhere} from "typeorm"
 import {EntityTarget} from "typeorm/common/EntityTarget"
+import {ColumnMetadata} from 'typeorm/metadata/ColumnMetadata'
 
 
 export interface EntityClass<T> {
@@ -72,13 +73,48 @@ export class Store {
                 for (let i = 1; i < e.length; i++) {
                     assert(entityClass === e[i].constructor, 'mass saving allowed only for entities of the same class')
                 }
-                for (let b of splitIntoBatches(e, 1000)) {
-                    await em.upsert(entityClass, b as any, ['id'])
-                }
+                await this.saveMany(em, entityClass, e)
             } else {
                 await em.upsert(e.constructor as EntityClass<E>, e as any, ['id'])
             }
         })
+    }
+
+    private async saveMany(em: EntityManager, entityClass: EntityClass<any>, entities: any[]): Promise<void> {
+        assert(entities.length > 0)
+        let metadata = em.connection.getMetadata(entityClass)
+        let fk = metadata.columns.filter(c => c.relationMetadata)
+        if (fk.length == 0) return this.upsertMany(em, entityClass, entities)
+        let currentSignature = this.getFkSignature(fk, entities[0])
+        let batch = []
+        for (let e of entities) {
+            let sig = this.getFkSignature(fk, e)
+            if (sig === currentSignature) {
+                batch.push(e)
+            } else {
+                await this.upsertMany(em, entityClass, batch)
+                currentSignature = sig
+                batch = [e]
+            }
+        }
+        if (batch.length) {
+            await this.upsertMany(em, entityClass, batch)
+        }
+    }
+
+    private getFkSignature(fk: ColumnMetadata[], entity: any): bigint {
+        let sig = 0n
+        for (let i = 0; i < fk.length; i++) {
+            let bit = fk[i].getEntityValue(entity) === undefined ? 0n : 1n
+            sig |= (bit << BigInt(i))
+        }
+        return sig
+    }
+
+    private async upsertMany(em: EntityManager, entityClass: EntityClass<any>, entities: any[]): Promise<void> {
+        for (let b of splitIntoBatches(entities, 1000)) {
+            await em.upsert(entityClass, b as any, ['id'])
+        }
     }
 
     /**
