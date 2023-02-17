fab701bf5cdb2e1e42c197d5f6d80c1574ae8724
Alexey Safonov <safonov.a.r@mail.ru>
2023-02-17 10:53:38 +0300

    Ethereum executed (#160)
    Co-authored-by: Alexander Belopashentsev <abelopashentsev@gmail.com>
    Co-authored-by: Eldar Gabdullin <eldargab@gmail.com>

--- test/astar-erc20/src/processor.ts
+++ test/astar-erc20/src/processor.ts
@@ -1,71 +1,93 @@
-import {SubstrateProcessor, toHex} from '@subsquid/substrate-processor'
+import {getTransaction, getTransactionResult} from '@subsquid/frontier'
+import {assertNotNull, SubstrateBatchProcessor} from '@subsquid/substrate-processor'
 import {TypeormDatabase} from '@subsquid/typeorm-store'
-import * as erc20 from './erc20'
-import {Transaction} from './model'
 import {toJSON} from '@subsquid/util-internal-json'
-import {getTransaction} from '@subsquid/substrate-frontier-evm'
+import * as erc20 from './abi/erc20'
+import {Transaction} from './model'
 
 
-const processor = new SubstrateProcessor(new TypeormDatabase())
+const processor = new SubstrateBatchProcessor()
+    .setDataSource({
+        chain: 'wss://public-rpc.pinknode.io/astar',
+        archive: 'https://astar.archive.subsquid.io/graphql',
+    })
+    .addEthereumTransaction([
+        '0x6a2d262D56735DbA19Dd70682B39F6bE9a931D98',
+        '0x3795C36e7D12A8c252A20C5a7B455f7c57b60283',
+    ])
 
 
-processor.setDataSource({
-    archive: 'https://astar.archive.subsquid.io/graphql',
-    chain: 'wss://public-rpc.pinknode.io/astar'
+processor.run(new TypeormDatabase(), async (ctx) => {
+    let transactions = []
+
+    for (let block of ctx.blocks) {
+        for (let item of block.items) {
+            if (item.name == 'Ethereum.Executed') {
+                let result = getTransactionResult(ctx, item.event)
+                if (result.status !== 'Succeed') {
+                    ctx.log.warn(result, `failed transaction`)
+                    continue
+                }
+
+                let call = assertNotNull(item.event.call)
+                let transaction = getTransaction(ctx, call)
+
+                let input = decodeTxInput(transaction.input)
+                if (input) transactions.push(
+                    new Transaction({
+                        id: call.id,
+                        block: block.header.height,
+                        timestamp: new Date(block.header.timestamp),
+                        txHash: transaction.hash,
+                        from: transaction.from,
+                        to: transaction.to,
+                        method: input.method,
+                        args: toJSON(input.args)
+                    })
+                )
+            }
+        }
+    }
+
+    await ctx.store.save(transactions)
 })
 
 
-processor.addEthereumTransactionHandler(
-    ['0x6a2d262D56735DbA19Dd70682B39F6bE9a931D98', '0x3795C36e7D12A8c252A20C5a7B455f7c57b60283'],
-    async ctx => {
-        let transaction = getTransaction(ctx, ctx.call)
-
-        let input = decodeInput(transaction.input)
-        if (!input) return
-
-        await ctx.store.save(new Transaction({
-            id: ctx.call.id,
-            block: ctx.block.height,
-            timestamp: new Date(ctx.block.timestamp),
-            txHash: transaction.hash,
-            from: transaction.from,
-            to: transaction.to,
-            type: transaction.type || 0,
-            input: toJSON(input)
-        }))
-    }
-)
-
-
-function decodeInput(input: string): {method: string, args: any[]} | undefined {
+function decodeTxInput(input: string): {method: string; args: any} | undefined {
     let sighash = input.slice(0, 10)
-
     switch (sighash) {
-        case erc20.functions['approve(address,uint256)'].sighash: {
-            const decoded = erc20.functions['approve(address,uint256)'].decode(input)
+        case erc20.functions.approve.sighash: {
+            let [spender, value] = erc20.functions.approve.decode(input)
             return {
                 method: 'approve',
-                args: [decoded[0], decoded[1].toBigInt()]
+                args: {
+                    spender,
+                    value: value.toBigInt()
+                }
             }
         }
-        case erc20.functions['transfer(address,uint256)'].sighash: {
-            const decoded = erc20.functions['transfer(address,uint256)'].decode(input)
+        case erc20.functions.transfer.sighash: {
+            let [to, value] = erc20.functions.transfer.decode(input)
             return {
                 method: 'transfer',
-                args: [decoded[0], decoded[1].toBigInt()]
+                args: {
+                    to,
+                    value: value.toBigInt()
+                }
             }
         }
-        case erc20.functions['transferFrom(address,address,uint256)'].sighash: {
-            const decoded = erc20.functions['transferFrom(address,address,uint256)'].decode(input)
+        case erc20.functions.transferFrom.sighash: {
+            let [from, to, value] = erc20.functions.transferFrom.decode(input)
             return {
                 method: 'transferFrom',
-                args: [decoded[0], decoded[0], decoded[2].toBigInt()]
+                args: {
+                    from,
+                    to,
+                    value: value.toBigInt()
+                }
             }
         }
         default:
             return undefined
     }
 }
-
-
-processor.run()
