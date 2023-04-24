1dcd088389ff51d8ffd298ddcf1ccd18137a63a5
Nikita Klimin <nklimin007@gmail.com>
2023-04-24 14:28:46 +0300

    fix: sort typesBundles

--- substrate/substrate-metadata/src/io.ts
+++ substrate/substrate-metadata/src/io.ts
@@ -5,40 +5,33 @@ import {eliminatePolkadotjsTypesBundle} from './old/typesBundle-polkadotjs'
 
 export function getOldTypesBundle(chain: string): OldTypesBundle | undefined {
     switch(chain) {
+        case 'acala':
+        case 'karura':
+            return require('./old/definitions/acala').bundle
         case 'aleph-node':
             return require('./old/definitions/aleph-node').bundle
         case 'altair':
             return require('./old/definitions/altair').bundle
-        case 'bifrost':
-            return require('./old/definitions/bifrost').bundle
-        case 'khala':
-            return require('./old/definitions/khala').bundle
-        case 'kusama':
-            return require('./old/definitions/kusama').bundle
-        case 'moonbeam':
-        case 'moonbase':
-        case 'moonriver':
-            return require('./old/definitions/moonbeam').bundle
-        case 'polkadot':
-            return require('./old/definitions/polkadot').bundle
-        case 'acala':
-        case 'karura':
-            return require('./old/definitions/acala').bundle
         case 'astar':
             return require('./old/definitions/astar').bundle
-        case 'shiden':
-            return require('./old/definitions/shiden').bundle
-        case 'shibuya':
-            return require('./old/definitions/shibuya').bundle
+        case 'bifrost':
+            return require('./old/definitions/bifrost').bundle
+        case 'basilisk':
+            return require('./old/definitions/basilisk').bundle
+        case 'calamari':
+            return require('./old/definitions/calamari').bundle
+        case 'clover':
+            return require('./old/definitions/clover').bundle
         case 'crust':
             return require('./old/definitions/crust').bundle
-        case 'shell':
-            return require('./old/definitions/shell').bundle
-        case 'statemint':
-        case 'statemine':
-            return require('./old/definitions/statemint').bundle
-        case 'subsocial':
-            return require('./old/definitions/subsocial').bundle
+        case 'darwinia':
+        case 'Darwinia': // real spec name
+            return require('./old/definitions/darwinia').bundle
+        case 'hydradx':
+        case 'hydra-dx': // real spec name
+            return require('./old/definitions/hydradx').bundle
+        case 'khala':
+            return require('./old/definitions/khala').bundle
         case 'kilt':
         case 'kilt-spiritnet':
         case 'kilt-spiritnet-develop':
@@ -47,33 +40,40 @@ export function getOldTypesBundle(chain: string): OldTypesBundle | undefined {
         case 'kilt-peregrine-develop':
         case 'kilt-mashnet':
             return require('./old/definitions/kilt').bundle
-        case 'hydradx':
-        case 'hydra-dx': // real spec name
-            return require('./old/definitions/hydradx').bundle
-        case 'pioneer':
-            return require('./old/definitions/pioneer').bundle
-        case 'parallel':
-        case 'heiko':
-            return require('./old/definitions/parallel').bundle
-        case 'clover':
-            return require('./old/definitions/clover').bundle
-        case 'manta':
-            return require('./old/definitions/manta').bundle
-        case 'calamari':
-            return require('./old/definitions/calamari').bundle
-        case 'basilisk':
-            return require('./old/definitions/basilisk').bundle
-        case 'unique':
-        case 'quartz':
-            return require('./old/definitions/unique').bundle
-        case 'darwinia':
-        case 'Darwinia': // real spec name
-            return require('./old/definitions/darwinia').bundle
         case 'kintsugi':
         case 'kintsugi-parachain': // real spec name
             return require('./old/definitions/kintsugi').bundle
+        case 'kusama':
+            return require('./old/definitions/kusama').bundle
+        case 'moonbeam':
+        case 'moonbase':
+        case 'moonriver':
+            return require('./old/definitions/moonbeam').bundle
+        case 'manta':
+            return require('./old/definitions/manta').bundle
+        case 'parallel':
+        case 'heiko':
+            return require('./old/definitions/parallel').bundle
+        case 'pioneer':
+            return require('./old/definitions/pioneer').bundle
+        case 'polkadot':
+            return require('./old/definitions/polkadot').bundle
+        case 'shiden':
+            return require('./old/definitions/shiden').bundle
+        case 'shibuya':
+            return require('./old/definitions/shibuya').bundle
+        case 'shell':
+            return require('./old/definitions/shell').bundle
+        case 'statemint':
+        case 'statemine':
+            return require('./old/definitions/statemint').bundle
+        case 'subsocial':
+            return require('./old/definitions/subsocial').bundle
         case 'reef':
             return require('./old/definitions/reef').bundle
+        case 'unique':
+        case 'quartz':
+            return require('./old/definitions/unique').bundle
         default:
             return undefined
     }
