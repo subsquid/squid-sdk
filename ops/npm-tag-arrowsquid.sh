13a9d27ae4e8dd0446ce2d1b8c1e3d4b16720504
Eldar Gabdullin <eldargab@gmail.com>
2023-06-15 13:17:22 +0300

    try to debug weird workflow error

--- ops/npm-tag-arrowsquid.sh
+++ ops/npm-tag-arrowsquid.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+echo "Begin ArrowSquid tagging"
+
 packages=$(node common/scripts/install-run-rush.js -q list --json \
     | jq '.projects[] | select(.versionPolicyName == "npm" and (.path | contains("substrate") | not)) | (.name + "@" + .version)' -r)
 
 for pkg in $packages; do
-    npm dist-tag add "$pkg" latest
+    npm dist-tag add "$pkg" latest || exit 1
 done
