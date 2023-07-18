719ad6f7cf8c4c47fd072171b908fb3e8cccbca6
Eldar Gabdullin <eldargab@gmail.com>
2023-07-19 01:20:50 +0300

    Revert "configure s3fs region via env variable"
    AWS SDK already supports AWS_REGION
    
    This reverts commit fe28f8c2e3074cba123061c07ceb8e496002ad55.

--- util/util-internal-fs/src/factory.ts
+++ util/util-internal-fs/src/factory.ts
@@ -21,7 +21,6 @@ export function createFs(url: string): Fs {
 
 function createS3Fs(root: string): S3Fs {
     let client = new S3Client({
-        region: process.env.AWS_DEFAULT_REGION,
         endpoint: process.env.AWS_S3_ENDPOINT
     })
     return new S3Fs({
