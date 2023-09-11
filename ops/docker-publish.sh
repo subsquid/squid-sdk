4dbe3548d7fff6c5534e07fea39a19ba8b767c16
Eldar Gabdullin <eldargab@gmail.com>
2023-09-11 23:22:52 +0300

    add substrate-metadata-service docker build

--- ops/docker-publish.sh
+++ ops/docker-publish.sh
@@ -24,5 +24,6 @@ function publish() {
 
 publish substrate/substrate-dump || exit 1
 publish substrate/substrate-ingest || exit 1
+publish substrate/substrate-metadata-service || exit 1
 
 #git push origin "HEAD:release/${release}" --follow-tags --verbose
