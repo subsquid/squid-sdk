80f228de631e9ec21c9866466e54d35846c2a98d
Eldar Gabdullin <eldargab@gmail.com>
2023-10-19 10:09:07 +0300

    promote ArrowSquid to latest

--- ops/npm-tag-arrowsquid.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-packages=$(node common/scripts/install-run-rush.js -q list --json \
-    | jq '.projects[] | select(.versionPolicyName == "npm" and (.path | contains("substrate") | not)) | (.name + "@" + .version)' -r)
-
-for pkg in $packages; do
-    npm dist-tag add "$pkg" latest || exit 1
-done
