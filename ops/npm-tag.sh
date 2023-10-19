ecb690701983fff6233fb26a5c9ab91ee43c6c67
Eldar Gabdullin <eldargab@gmail.com>
2023-10-19 15:54:48 +0300

    debug tag action

--- ops/npm-tag.sh
+++ ops/npm-tag.sh
@@ -6,5 +6,5 @@ packages=$(node common/scripts/install-run-rush.js -q list --json \
     | jq '.projects[] | select(.versionPolicyName == "npm") | (.name + "@" + .version)' -r)
 
 for pkg in $packages; do
-    npm dist-tag add "$pkg" "$tag"
+    npm dist-tag add "$pkg" "$tag" || exit 1
 done
