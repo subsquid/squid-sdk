351417fcc5ef7fb8828af96bd8d2523bc8502d6d
Eldar Gabdullin <eldargab@gmail.com>
2024-01-02 15:03:20 +0300

    fixing tag workflow

--- ops/npm-tag.sh
+++ ops/npm-tag.sh
@@ -2,6 +2,10 @@
 
 tag="$1"
 
+# First run test package discovery that will print garbage to stdout anyway
+node common/scripts/install-run-rush.js -q list || exit 1
+
+# Then run real package discovery
 packages=$(node common/scripts/install-run-rush.js -q list --json \
     | jq '.projects[] | select(.versionPolicyName == "npm") | (.name + "@" + .version)' -r)
 
