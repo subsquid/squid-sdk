f9525e741a88f7534d41fbe9d42d6fe643cb06c0
Eldar Gabdullin <eldargab@gmail.com>
2024-01-17 10:41:04 +0300

    log parser

--- solana/solana-data/src/normalization/log-parser.ts
+++ solana/solana-data/src/normalization/log-parser.ts
@@ -1,65 +1,160 @@
+import {unexpectedCase} from '@subsquid/util-internal'
 import {Base58Bytes} from '../base'
 
 
-interface TokenBase {
-    message: string
-}
-
-
-interface InvokeToken extends TokenBase {
+interface InvokeToken {
     kind: 'invoke'
     programId: Base58Bytes
     stackHeight: number
 }
 
 
-interface InvokeResult extends TokenBase {
+interface InvokeResultToken {
     kind: 'invoke-result'
     programId: Base58Bytes
     success: boolean
 }
 
 
-interface Message extends TokenBase {
+interface MessageToken {
     kind: 'message'
 }
 
 
-export type Token = InvokeToken | InvokeResult | Message
+export type Token = InvokeToken | InvokeResultToken | MessageToken
 
 
-export function toToken(message: string): Token {
-    let m = /^Program ([123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+) invoke \[(\d+)]$/.exec(message)
+export function toToken(msg: string): Token {
+    let m = /^Program ([123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+) invoke \[(\d+)]$/.exec(msg)
     if (m) return {
         kind: 'invoke',
         programId: m[1],
-        stackHeight: parseInt(m[2]),
-        message
+        stackHeight: parseInt(m[2])
     }
 
-    m = /^Program ([123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+) success$/.exec(message)
+    m = /^Program ([123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+) success$/.exec(msg)
     if (m) return {
         kind: 'invoke-result',
         programId: m[1],
-        success: true,
-        message
+        success: true
     }
 
-    m = /^Program ([123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+) failed/.exec(message)
+    m = /^Program ([123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]+) failed/.exec(msg)
     if (m) return {
         kind: 'invoke-result',
         programId: m[1],
-        success: false,
-        message
+        success: false
     }
 
     return {
-        kind: 'message',
-        message
+        kind: 'message'
     }
 }
 
 
-export interface LogItem {
+export interface InstructionRecord {
+    success: boolean
+    stackHeight: number
     programId: Base58Bytes
+    log: (string | InstructionRecord)[]
+}
+
+
+const ParseFailure = new Error('parsing failure')
+
+
+export class LogParser {
+    private pos = 0
+    private records: InstructionRecord[] = []
+    private success = true
+    private failure?: string
+
+    constructor(private messages: string[]) {
+        try {
+            this.parse()
+        } catch(err: any) {
+            if (err === ParseFailure) {
+                this.success = false
+            } else {
+                throw err
+            }
+        }
+    }
+
+    ok(): boolean {
+        return this.success
+    }
+
+    getError(): string | undefined {
+        return this.failure
+    }
+
+    getResult(): InstructionRecord[] {
+        return this.records
+    }
+
+    getPos(): number {
+        return this.pos
+    }
+
+    private parse(): void {
+        while (this.pos < this.messages.length) {
+            let token = toToken(this.message())
+            this.assert(token.kind === 'invoke', 'invoke message expected')
+            this.pos += 1
+            this.records.push(this.invoke(token))
+        }
+    }
+
+    private invoke(invoke: InvokeToken): InstructionRecord {
+        let log: (string | InstructionRecord)[] = []
+        let result: InvokeResultToken
+
+        LOOP: while (true) {
+            let msg = this.message()
+            let token = toToken(msg)
+            switch(token.kind) {
+                case 'message':
+                    this.pos += 1
+                    log.push(msg)
+                    break
+                case 'invoke':
+                    this.assert(
+                        token.stackHeight > invoke.stackHeight,
+                        'child invoke should have greater stack height '
+                    )
+                    this.pos += 1
+                    log.push(this.invoke(token))
+                    break
+                case 'invoke-result':
+                    this.assert(
+                        invoke.programId === token.programId,
+                        'invoke result program does not match invoked program'
+                    )
+                    this.pos += 1
+                    result = token
+                    break LOOP
+                default:
+                    throw unexpectedCase()
+            }
+        }
+
+        return {
+            success: result.success,
+            stackHeight: invoke.stackHeight,
+            programId: invoke.programId,
+            log
+        }
+    }
+
+    private message(): string {
+        this.assert(this.pos < this.messages.length, 'unexpected end of messages')
+        return this.messages[this.pos]
+    }
+
+    private assert(value: unknown, msg: string): asserts value {
+        if (value) return
+        this.failure = msg
+        throw ParseFailure
+    }
 }
--- solana/solana-data/src/test.ts
+++ solana/solana-data/src/test.ts
@@ -1,34 +1,130 @@
-import {createLogger} from '@subsquid/logger'
-import {RpcClient} from '@subsquid/rpc-client'
-import {addErrorContext, assertNotNull, runProgram} from '@subsquid/util-internal'
-import {mapRpcBlock} from './normalization'
-import {getBlockCtx, RpcDataSource} from './rpc'
+import assert from 'assert'
+import {describe, it} from 'node:test'
+import {LogParser} from './normalization/log-parser'
 
 
-const log = createLogger('solana')
-
-
-runProgram(async () => {
-    let client = new RpcClient({
-        url: assertNotNull(process.env.SOLANA_NODE),
-        retryAttempts: 100,
-        log
-    })
-
-    let src = new RpcDataSource({
-        rpc: client
-    })
-
-    for await (let batch of src.getFinalizedBlocks([{
-        range: {from: 200_000_000, to: 200_010_000},
-        request: {transactions: true, rewards: false}
-    }])) {
-        for (let block of batch.blocks) {
-            try {
-                console.log(JSON.stringify(mapRpcBlock(block)))
-            } catch(err: any) {
-                throw addErrorContext(err, getBlockCtx(block))
+describe('log parser', function() {
+    it('real failed tx case', function() {
+        let parser = new LogParser([
+            "Program ComputeBudget111111111111111111111111111111 invoke [1]",
+            "Program ComputeBudget111111111111111111111111111111 success",
+            "Program GVXG8ciCwhoqP6BEpPsGb8Q5JzBqFs8CkQrZv3rLXmXi invoke [1]",
+            "Program log: Xa nhau lau nay em quen ai chua. Co ai yeu em nhu anh khi xua ???",
+            "Program 675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8 invoke [2]",
+            "Program log: ray_log: AwBlzR0AAAAAAAAAAAAAAAABAAAAAAAAAEu/2B0AAAAAbe4RB0RRBgBRRjF0EgAAABefogAdCgAA",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA invoke [3]",
+            "Program log: Instruction: Transfer",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 341802 compute units",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA success",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA invoke [3]",
+            "Program log: Instruction: Transfer",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 334250 compute units",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA success",
+            "Program 675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8 consumed 119245 of 448044 compute units",
+            "Program 675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8 success",
+            "Program 9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP invoke [2]",
+            "Program log: Instruction: Swap",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA invoke [3]",
+            "Program log: Instruction: Transfer",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 280381 compute units",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA success",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA invoke [3]",
+            "Program log: Instruction: MintTo",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4492 of 244311 compute units",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA success",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA invoke [3]",
+            "Program log: Instruction: Transfer",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 236861 compute units",
+            "Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA success",
+            "Program 9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP consumed 67095 of 298680 compute units",
+            "Program 9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP success",
+            "Program log: panicked at 'Dau vi nhau vay la qua du roi. Dung tim nhau nua !!!', src\\processor.rs:79:13",
+            "Program GVXG8ciCwhoqP6BEpPsGb8Q5JzBqFs8CkQrZv3rLXmXi consumed 271089 of 500000 compute units",
+            "Program failed to complete: BPF program panicked",
+            "Program GVXG8ciCwhoqP6BEpPsGb8Q5JzBqFs8CkQrZv3rLXmXi failed: Program failed to complete"
+        ])
+        assert.strictEqual(parser.getError(), undefined)
+        assert.deepStrictEqual(parser.getResult(), [
+            {
+                success: true,
+                stackHeight: 1,
+                programId: 'ComputeBudget111111111111111111111111111111',
+                log: []
+            },
+            {
+                success: false,
+                stackHeight: 1,
+                programId: 'GVXG8ciCwhoqP6BEpPsGb8Q5JzBqFs8CkQrZv3rLXmXi',
+                log: [
+                    'Program log: Xa nhau lau nay em quen ai chua. Co ai yeu em nhu anh khi xua ???',
+                    {
+                        success: true,
+                        stackHeight: 2,
+                        programId: '675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8',
+                        log: [
+                            'Program log: ray_log: AwBlzR0AAAAAAAAAAAAAAAABAAAAAAAAAEu/2B0AAAAAbe4RB0RRBgBRRjF0EgAAABefogAdCgAA',
+                            {
+                                success: true,
+                                stackHeight: 3,
+                                programId: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+                                log: [
+                                    'Program log: Instruction: Transfer',
+                                    'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 341802 compute units'
+                                ]
+                            },
+                            {
+                                success: true,
+                                stackHeight: 3,
+                                programId: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+                                log: [
+                                    'Program log: Instruction: Transfer',
+                                    'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 334250 compute units'
+                                ]
+                            },
+                            'Program 675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8 consumed 119245 of 448044 compute units'
+                        ]
+                    },
+                    {
+                        success: true,
+                        stackHeight: 2,
+                        programId: '9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP',
+                        log: [
+                            'Program log: Instruction: Swap',
+                            {
+                                success: true,
+                                stackHeight: 3,
+                                programId: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+                                log: [
+                                    'Program log: Instruction: Transfer',
+                                    'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 280381 compute units'
+                                ]
+                            },
+                            {
+                                success: true,
+                                stackHeight: 3,
+                                programId: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+                                log: [
+                                    'Program log: Instruction: MintTo',
+                                    'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4492 of 244311 compute units'
+                                ]
+                            },
+                            {
+                                success: true,
+                                stackHeight: 3,
+                                programId: 'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA',
+                                log: [
+                                    'Program log: Instruction: Transfer',
+                                    'Program TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA consumed 4645 of 236861 compute units'
+                                ]
+                            },
+                            'Program 9W959DqEETiGZocYWCQPaJ6sBmUzgfxXfqGeTEdp3aQP consumed 67095 of 298680 compute units'
+                        ]
+                    },
+                    "Program log: panicked at 'Dau vi nhau vay la qua du roi. Dung tim nhau nua !!!', src\\processor.rs:79:13",
+                    'Program GVXG8ciCwhoqP6BEpPsGb8Q5JzBqFs8CkQrZv3rLXmXi consumed 271089 of 500000 compute units',
+                    'Program failed to complete: BPF program panicked'
+                ]
             }
-        }
-    }
-}, err => log.fatal(err))
+        ])
+    })
+})
