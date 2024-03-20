cdaf232b7d7db1cdc7b9ca6de68151ba1cf16340
ivan <ivanruch@gmail.com>
2024-03-21 00:53:28 +0100

    Add tuples, benchmark with viem

--- evm/evm-utils/src/codecs/struct.ts
+++ evm/evm-utils/src/codecs/struct.ts
@@ -1,61 +1,88 @@
-import { Codec, GetCodecType } from "../codec";
+import { Codec } from "../codec";
 import { Sink } from "../sink";
 import { Src } from "../src";
-import { AddOptionToUndefined, Simplify } from "../type-util";
-import { propAccess, propName } from "../util";
-
-export type GetStructType<Props> = Simplify<
-  AddOptionToUndefined<{
-    [K in keyof Props]: GetCodecType<Props[K]>;
-  }>
->;
 
 export type Props<S> = {
-  [K in keyof S]: Codec<S[K]>;
+  [K in keyof S]: { codec: Codec<S[K]>; index: number };
 };
 
 export class StructCodec<S> implements Codec<S> {
-  public isDynamic = false;
-  encode: (sink: Sink, val: S) => void;
-  decode: (src: Src) => S;
-
+  public isDynamic: boolean;
+  private readonly sortedProps: {
+    name: string;
+    codec: Codec<any>;
+    index: number;
+  }[];
   constructor(public readonly props: Props<S>) {
-    this.decode = this.createDecode();
-    this.encode = this.createEncode();
+    this.sortedProps = Object.entries(props)
+      .map(([key, value]: [string, any]) => ({
+        name: key,
+        ...value,
+      }))
+      .sort((a, b) => a.index - b.index);
+    this.isDynamic = this.sortedProps.some((p) => p.codec.isDynamic);
   }
 
-  private createDecode(): any {
-    let body = "return {\n";
-    for (let key in this.props) {
-      body += `${propName(key)}: this.props${propAccess(key)}.decode(src),\n`;
+  public encode(sink: Sink, val: S): void {
+    if (this.isDynamic) {
+      this.encodeDynamic(sink, val);
+      return;
     }
-    body += "}\n";
-    return new Function("src", body);
+    for (let prop of this.sortedProps) {
+      prop.codec.encode(sink, (val as any)[prop.name]);
+    }
   }
 
-  private createEncode(): any {
-    let body = ``;
-    for (let key in this.props) {
-      let a = propAccess(key);
-      body += `this.props${a}.encode(sink, val${a})\n`;
+  private encodeDynamic(sink: Sink, val: S): void {
+    sink.offset();
+    const tempSink = new Sink(this.sortedProps.length);
+    for (const prop of this.sortedProps) {
+      prop.codec.encode(tempSink, (val as any)[prop.name]);
     }
-    console.log(body);
-    return new Function("sink", "val", body);
+    sink.append(tempSink);
+    sink.jumpBack();
   }
+
+  public decode(src: Src): S {
+    if (this.isDynamic) {
+      return this.decodeDynamic(src);
+    }
+    let result: any = {};
+    for (const prop of this.sortedProps) {
+      result[prop.name] = prop.codec.decode(src);
+    }
+    return result;
+  }
+
+  private decodeDynamic(src: Src): S {
+    let result: any = {};
+
+    const offset = src.u32();
+    const tmpSrc = src.slice(offset);
+    for (const prop of this.sortedProps) {
+      result[prop.name] = prop.codec.decode(tmpSrc);
+    }
+    return result;
+  }
+
+  // private createDecode(): any {
+  //   let body = "return {\n";
+  //   for (let key in this.props) {
+  //     body += `${propName(key)}: this.props${propAccess(key)}.decode(src),\n`;
+  //   }
+  //   body += "}\n";
+  //   return new Function("src", body);
+  // }
+  //
+  //   private createEncode(): any {
+  //     let body = `
+  //
+  // `;
+  //     for (let key in this.props) {
+  //       let a = propAccess(key);
+  //       body += `this.props${a}.encode(sink, val${a})\n`;
+  //     }
+  //     return new Function("sink", "val", body);
+  //   }
+  // }
 }
-
-// export class StructCodec<S> implements Codec<S> {
-//     constructor(public readonly props: Props<S>) {}
-//
-//     encode(sink: Sink, val: S): void {
-//         throw new Error('Method not implemented.')
-//     }
-//
-//     decode(src: Src): S {
-//         let result: any = {}
-//         for (let key in this.props) {
-//             result[key] = this.props[key].decode(src)
-//         }
-//         return result
-//     }
-// }
--- evm/evm-utils/test/struct.test.ts
+++ evm/evm-utils/test/struct.test.ts
@@ -15,22 +15,23 @@ function compareTypes(sink: Sink, types: AbiParameter[], values: any[]) {
 }
 
 describe("StructCodec", () => {
-  it("should work", () => {
+  it("static tuple", () => {
     const s = struct({
-      a: int8,
-      b: uint256,
-      c: struct({
-        // d: array(uint256),
-        e: address,
-      }),
+      a: { codec: int8, index: 0 },
+      b: { codec: uint256, index: 1 },
+      c: {
+        codec: struct({
+          e: { codec: address, index: 0 },
+        }),
+        index: 2,
+      },
     });
 
-    const sink = new Sink(1);
+    const sink = new Sink(3);
     s.encode(sink, {
       a: 1,
       b: 2n,
       c: {
-        // d: [3n, 4n],
         e: "0x1234567890123456789012345678901234567890",
       },
     });
@@ -46,10 +47,7 @@ describe("StructCodec", () => {
             {
               name: "c",
               type: "tuple",
-              components: [
-                // { name: "d", type: "uint256[]" },
-                { name: "e", type: "address" },
-              ],
+              components: [{ name: "e", type: "address" }],
             },
           ],
         },
@@ -65,5 +63,76 @@ describe("StructCodec", () => {
         },
       ]
     );
+
+    expect(s.decode(new Src(sink.result()))).toStrictEqual({
+      a: 1,
+      b: 2n,
+      c: {
+        e: "0x1234567890123456789012345678901234567890",
+      },
+    });
+  });
+
+  it("dynamic tuple", () => {
+    const s = struct({
+      a: { codec: array(uint256), index: 0 },
+      b: { codec: uint256, index: 1 },
+      c: {
+        codec: struct({
+          d: { codec: array(uint256), index: 0 },
+          e: { codec: address, index: 1 },
+        }),
+        index: 2,
+      },
+    });
+
+    const sink = new Sink(1);
+    s.encode(sink, {
+      a: [100n, 1n, 123n],
+      b: 2n,
+      c: {
+        d: [3n, 4n],
+        e: "0x1234567890123456789012345678901234567890",
+      },
+    });
+    compareTypes(
+      sink,
+      [
+        {
+          type: "tuple",
+          components: [
+            { name: "a", type: "uint256[]" },
+            { name: "b", type: "uint256" },
+            {
+              name: "c",
+              type: "tuple",
+              components: [
+                { name: "d", type: "uint256[]" },
+                { name: "e", type: "address" },
+              ],
+            },
+          ],
+        },
+      ],
+      [
+        {
+          a: [100n, 1n, 123n],
+          b: 2n,
+          c: {
+            d: [3n, 4n],
+            e: "0x1234567890123456789012345678901234567890",
+          },
+        },
+      ]
+    );
+
+    expect(s.decode(new Src(sink.result()))).toStrictEqual({
+      a: [100n, 1n, 123n],
+      b: 2n,
+      c: {
+        d: [3n, 4n],
+        e: "0x1234567890123456789012345678901234567890",
+      },
+    });
   });
 });
