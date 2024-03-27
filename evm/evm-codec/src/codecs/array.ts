550ef9690327e029841b2b52c0e028930758cac3
Eldar Gabdullin <eldargab@gmail.com>
2024-03-27 13:39:44 +0300

    Revert "new Src()"
    This reverts commit 15a2aded0aa3a71c30f8a91139e8edc089701586.

--- evm/evm-codec/src/codecs/array.ts
+++ evm/evm-codec/src/codecs/array.ts
@@ -25,11 +25,12 @@ export class ArrayCodec<const T> implements Codec<readonly T[]> {
     src.safeJump(offset);
     const len = src.u32();
 
-    const tmpSrc = src.slice(offset + WORD_SIZE);
+    src.pushSlice(offset + WORD_SIZE);
     const val = new Array(len);
     for (let i = 0; i < val.length; i++) {
-      val[i] = this.item.decode(tmpSrc);
+      val[i] = this.item.decode(src);
     }
+    src.popSlice()
     src.jumpBack();
     return val;
   }
@@ -80,11 +81,12 @@ export class FixedArrayCodec<const T> implements Codec<readonly T[]> {
 
   private decodeDynamic(src: Src): T[] {
     const offset = src.u32();
-    const tmpSrc = src.slice(offset);
+    src.pushSlice(offset);
     let val = new Array(this.size);
     for (let i = 0; i < val.length; i++) {
-      val[i] = this.item.decode(tmpSrc);
+      val[i] = this.item.decode(src);
     }
+    src.popSlice()
     return val;
   }
 }
