a86135273f32a58602d03e47408c7f3ed222445e
ivan <ivanruch@gmail.com>
2024-05-20 20:22:37 +0200

    Fix safe number conversion

--- evm/evm-codec/src/safeToNumber.ts
+++ evm/evm-codec/src/safeToNumber.ts
@@ -1,5 +1,5 @@
 export function safeToNumber(value: number | bigint): number {
-  if (!Number.isSafeInteger(value)) {
+  if ( value < Number.MIN_SAFE_INTEGER || value > Number.MAX_SAFE_INTEGER) {
     throw new Error(`${value} is not a safe integer`)
   }
   return Number(value)
