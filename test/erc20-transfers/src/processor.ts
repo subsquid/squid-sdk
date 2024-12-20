cc6baca8ee48c766e96f8858ef5d6cd8bd893478
belopash <abelopashentsev@gmail.com>
2024-12-20 21:15:53 +0300

    use ReadableStream for portal client stream

--- test/erc20-transfers/src/processor.ts
+++ test/erc20-transfers/src/processor.ts
@@ -10,12 +10,12 @@ const CONTRACT = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'.toLowerCase()
 const processor = new EvmBatchProcessor()
     .setPortal({
         url: 'https://portal.sqd.dev/datasets/ethereum-mainnet',
-        bufferThreshold: 100 * 1024 * 1024,
+        bufferThreshold: 10 * 1024 * 1024,
         newBlockTimeout: 5000,
     })
     .setRpcEndpoint('https://rpc.ankr.com/eth')
     .setFinalityConfirmation(500)
-    .setBlockRange({from: 20801368})
+    .setBlockRange({from: 0})
     .setFields({
         block: {size: true},
         log: {transactionHash: true},
@@ -32,20 +32,20 @@ processor.run(new TypeormDatabase({supportHotBlocks: true}), async ctx => {
     for (let block of ctx.blocks) {
         for (let log of block.logs) {
             if (log.address == CONTRACT &&  erc20.events.Transfer.is(log)) {
-                // let {from, to, value} = erc20.events.Transfer.decode(log)
+                let {from, to, value} = erc20.events.Transfer.decode(log)
                 transfers.push(new Transfer({
-                    // id: log.id,
-                    // blockNumber: block.header.height,
-                    // timestamp: new Date(block.header.timestamp),
-                    // tx: log.transactionHash,
-                    // from,
-                    // to,
-                    // amount: value
+                    id: log.id,
+                    blockNumber: block.header.height,
+                    timestamp: new Date(block.header.timestamp),
+                    tx: log.transactionHash,
+                    from,
+                    to,
+                    amount: value
                 }))
             }
         }
     }
 
     ctx.log.info(`found ${transfers.length} transfers`)
-    // await ctx.store.insert(transfers)
+    await ctx.store.insert(transfers)
 })
--- util/portal-client/src/client.ts
+++ util/portal-client/src/client.ts
@@ -1,6 +1,6 @@
 import {HttpClient} from '@subsquid/http-client'
 import type {Logger} from '@subsquid/logger'
-import {AsyncQueue, ensureError, last, wait, withErrorContext} from '@subsquid/util-internal'
+import {AsyncQueue, last, wait, withErrorContext} from '@subsquid/util-internal'
 import {splitLines} from '@subsquid/util-internal-archive-layout'
 import {addTimeout, TimeoutError} from '@subsquid/util-timeout'
 import assert from 'assert'
@@ -29,14 +29,20 @@ export interface PortalClientOptions {
     requestTimeout?: number
     retryAttempts?: number
     bufferThreshold?: number
+    newBlockThreshold?: number
+    durationThreshold?: number
     newBlockTimeout?: number
+    headPollInterval?: number
 }
 
 export class PortalClient {
     private url: URL
     private http: HttpClient
     private requestTimeout: number
+    private headPollInterval: number
     private bufferThreshold: number
+    private newBlockThreshold: number
+    private durationThreshold: number
     private newBlockTimeout: number
     private retryAttempts: number
     private log?: Logger
@@ -46,7 +52,10 @@ export class PortalClient {
         this.log = options.log
         this.http = options.http
         this.requestTimeout = options.requestTimeout ?? 180_000
+        this.headPollInterval = options.headPollInterval ?? 5_000
         this.bufferThreshold = options.bufferThreshold ?? 10 * 1024 * 1024
+        this.newBlockThreshold = options.newBlockThreshold ?? 500
+        this.durationThreshold = options.durationThreshold ?? 5_000
         this.newBlockTimeout = options.newBlockTimeout ?? 120_000
         this.retryAttempts = options.retryAttempts ?? Infinity
     }
@@ -104,133 +113,209 @@ export class PortalClient {
             })
     }
 
-    async *finalizedStream<B extends Block = Block, Q extends PortalQuery = PortalQuery>(
+    finalizedStream<B extends Block = Block, Q extends PortalQuery = PortalQuery>(
         query: Q,
         stopOnHead = false
-    ): AsyncIterable<B[]> {
-        let queue = new AsyncQueue<B[] | Error>(1)
-        let bufferSize = 0
-        let isReady = false
-        let cache: B[] = []
+    ): ReadableStream<B[]> {
+        let headPollInterval = this.headPollInterval
+        let newBlockThreshold = this.newBlockThreshold
+        let durationThreshold = this.durationThreshold
+        let bufferThreshold = this.bufferThreshold
+        let newBlockTimeout = this.newBlockTimeout
 
-        const getBuffer = () => {
-            if (queue.isClosed()) return
-            let peeked = queue.peek()
-            // FIXME: is it a valid case?
-            if (peeked instanceof Error) return
-
-            // buffer has been consumed, we need to reset
-            if (isReady && !peeked) {
-                reset()
-            }
-
-            return peeked ?? cache
-        }
-
-        const reset = () => {
-            bufferSize = 0
-            isReady = false
-            cache.length = 0
-        }
-
-        const setReady = () => {
-            if (queue.isClosed()) return
-            if (isReady) return
-            queue.forcePut(cache)
-            isReady = true
-            cache = []
-        }
-
-        const waitForReset = async () => {
-            if (queue.isClosed()) return
-            await queue.wait()
-            reset()
-        }
+        let buffer = new BlocksBuffer<B>(bufferThreshold)
+        let abortStream = new AbortController()
 
         const ingest = async () => {
-            let fromBlock = query.fromBlock
-            let toBlock = query.toBlock ?? Infinity
+            let startBlock = query.fromBlock
+            let endBlock = query.toBlock ?? Infinity
 
-            while (fromBlock <= toBlock) {
-                let archiveQuery = {...query, fromBlock}
+            let heartbeat: HeartBeat | undefined
+            let timeout: ReturnType<typeof setTimeout> | undefined
+            let reader: ReadableStreamDefaultReader<string[]> | undefined
 
-                let res = await this.http
-                    .request<Readable>('POST', this.getDatasetUrl(`finalized-stream`), {
-                        json: archiveQuery,
-                        retryAttempts: this.retryAttempts,
-                        httpTimeout: this.requestTimeout,
-                        stream: true,
-                    })
-                    .catch(
-                        withErrorContext({
-                            archiveQuery,
-                        })
-                    )
+            function abort() {
+                return reader?.cancel()
+            }
 
-                // no blocks left
-                if (res.status == 204) {
-                    if (stopOnHead) return
-
-                    await wait(1000)
-                    continue
-                }
-
+            while (startBlock <= endBlock && !abortStream.signal.aborted) {
                 try {
-                    let stream = splitLines(res.body)
+                    let archiveQuery = {...query, fromBlock: startBlock}
+                    let res = await this.http
+                        .request<Readable>('POST', this.getDatasetUrl('finalized-stream'), {
+                            json: archiveQuery,
+                            httpTimeout: this.requestTimeout,
+                            retryAttempts: this.retryAttempts,
+                            stream: true,
+                            abort: abortStream.signal,
+                        })
+                        .catch(
+                            withErrorContext({
+                                query: archiveQuery,
+                            })
+                        )
 
+                    if (res.status == 204) {
+                        if (stopOnHead) break
+                        await wait(headPollInterval, abortStream.signal)
+                        continue
+                    }
+
+                    abortStream.signal.addEventListener('abort', abort, {once: true})
+
+                    let heartbeatInterval = Math.ceil(newBlockThreshold / 4)
+                    heartbeat = new HeartBeat((diff) => {
+                        if (diff > newBlockThreshold) {
+                            buffer.ready()
+                        }
+                    }, heartbeatInterval)
+
+                    timeout = setTimeout(() => buffer.ready(), durationThreshold)
+
+                    let stream = addStreamTimeout(Readable.toWeb(res.body) as ReadableStream<Buffer>, newBlockTimeout)
+                    let reader = splitLines(stream)
                     while (true) {
-                        let lines = await addTimeout(stream.next(), this.newBlockTimeout)
+                        let lines = await reader.next()
                         if (lines.done) break
 
-                        let buffer = getBuffer()
-                        if (buffer == null) break
+                        heartbeat.pulse()
 
+                        let size = 0
                         let blocks = lines.value.map((line) => {
-                            bufferSize += line.length
-                            return JSON.parse(line) as B
+                            let block = JSON.parse(line) as B
+                            size += line.length
+                            return block
                         })
 
-                        // FIXME: won't it overflow stack?
-                        buffer.push(...blocks)
+                        await buffer.put(blocks, size)
 
-                        fromBlock = last(blocks).header.number + 1
-
-                        if (bufferSize > this.bufferThreshold) {
-                            setReady()
-                            await waitForReset()
-                        }
-                    }
-
-                    if (bufferSize > 0) {
-                        setReady()
+                        let lastBlock = last(blocks).header.number
+                        startBlock = lastBlock + 1
                     }
                 } catch (err) {
-                    if (err instanceof TimeoutError) {
-                        this.log?.warn(
-                            `resetting stream, because we haven't seen a new blocks for ${this.newBlockTimeout} ms`
-                        )
+                    if (abortStream.signal.aborted) {
+                        // FIXME: should we do anything here?
+                    } else if (err instanceof TimeoutError) {
+                        this.log?.warn(`resetting stream due to inactivity for ${this.newBlockTimeout} ms`)
                     } else {
                         throw err
                     }
                 } finally {
-                    // FIXME: is it needed?
-                    res.body.destroy()
+                    await reader?.cancel().catch(() => {})
+                    heartbeat?.stop()
+                    buffer.ready()
+                    clearTimeout(timeout)
+                    abortStream.signal.removeEventListener('abort', abort)
                 }
             }
         }
 
-        ingest().then(
-            () => queue.close(),
-            (err) => {
-                if (queue.isClosed()) return
-                queue.forcePut(ensureError(err))
-                queue.close()
-            }
-        )
+        return new ReadableStream({
+            start: async (controller) => {
+                ingest()
+                    .then(() => {
+                        buffer.close()
+                    })
+                    .catch((error) => {
+                        if (buffer.isClosed()) return
+                        controller.error(error)
+                        buffer.close()
+                    })
+            },
+            pull: async (controller) => {
+                let value = await buffer.take()
+                if (value) {
+                    controller.enqueue(value)
+                } else {
+                    controller.close()
+                }
+            },
+            cancel: () => {
+                abortStream.abort()
+            },
+        })
+    }
+}
 
-        for await (let valueOrError of queue.iterate()) {
-            if (valueOrError instanceof Error) throw valueOrError
-            yield valueOrError
+class BlocksBuffer<B extends Block> {
+    private blocks: B[] = []
+    private queue = new AsyncQueue<B[]>(1)
+    private size = 0
+
+    constructor(private bufferSizeThreshold: number) {}
+
+    async put(blocks: B[], size: number) {
+        this.blocks.push(...blocks)
+        this.size += size
+
+        if (this.size > this.bufferSizeThreshold) {
+            this.ready()
+            await this.queue.wait()
         }
     }
+
+    async take() {
+        let value = await this.queue.take()
+        this.blocks = []
+        this.size = 0
+        return value
+    }
+
+    ready() {
+        if (this.blocks.length == 0) return
+        this.queue.forcePut(this.blocks)
+    }
+
+    close() {
+        return this.queue.close()
+    }
+
+    isClosed() {
+        return this.queue.isClosed()
+    }
+}
+
+class HeartBeat {
+    private interval: ReturnType<typeof setInterval> | undefined
+    private timestamp: number
+
+    constructor(fn: (diff: number) => void, ms?: number) {
+        this.timestamp = Date.now()
+        this.interval = setInterval(() => fn(Date.now() - this.timestamp), ms)
+    }
+
+    pulse() {
+        this.timestamp = Date.now()
+    }
+
+    stop() {
+        clearInterval(this.interval)
+    }
+}
+
+function addStreamTimeout<T>(
+    stream: ReadableStream<T>,
+    ms: number,
+    onTimeout?: () => Error | undefined | void
+): ReadableStream<T> {
+    const reader = stream.getReader()
+
+    return new ReadableStream({
+        pull: async (c) => {
+            try {
+                let data = await addTimeout(reader.read(), ms, onTimeout)
+                if (data.done) {
+                    c.close()
+                } else {
+                    c.enqueue(data.value)
+                }
+            } catch (e) {
+                c.error(e)
+                await reader.cancel()
+            }
+        },
+        cancel: async (reason) => {
+            await reader.cancel(reason)
+        },
+    })
 }
