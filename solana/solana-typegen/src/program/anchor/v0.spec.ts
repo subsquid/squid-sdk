d5f5f99747018244d31fb5faa1ddba4607f2c5b4
Alexander Belopashentsev <61732514+belopash@users.noreply.github.com>
2025-01-15 18:39:57 +0300

    solana: add HashMap and HashSet types support (#367)

--- solana/solana-typegen/src/program/anchor/v0.spec.ts
+++ solana/solana-typegen/src/program/anchor/v0.spec.ts
@@ -238,6 +238,8 @@ export type IdlType =
     | IdlTypeCOption
     | IdlTypeVec
     | IdlTypeArray
+    | IdlTypeHashMap
+    | IdlTypeHashSet
     | IdlTypeDefined
     | IdlTypeGeneric
 
@@ -256,6 +258,14 @@ export type IdlTypeVec = {
 
 export type IdlTypeArray = {
     array: [idlType: IdlType, size: IdlArrayLen]
+}
+
+export type IdlTypeHashMap = {
+    hashMap: [idlType: IdlType, idlType: IdlType]
+}
+
+export type IdlTypeHashSet = {
+    hashSet: IdlType
 }
 
 export type IdlTypeDefined = {
@@ -394,6 +404,17 @@ function fromType(type: IdlType): Type {
         return {
             kind: TypeKind.Array,
             type: fromType(type.vec),
+        }
+    } else if ('hashMap' in type) {
+        return {
+            kind: TypeKind.HashMap,
+            key: fromType(type.hashMap[0]),
+            value: fromType(type.hashMap[1]),
+        }
+    } else if ('hashSet' in type) {
+        return {
+            kind: TypeKind.HashSet,
+            type: fromType(type.hashSet),
         }
     } else if ('defined' in type) {
         return {
