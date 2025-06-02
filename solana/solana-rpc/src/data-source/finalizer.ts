ae67f8aefbe4cbbf814bb6884753b9a1f769caaa
Eldar Gabdullin <eldargab@gmail.com>
2025-06-02 16:01:16 +0300

    rate limit finalization probes

--- solana/solana-rpc/src/data-source/finalizer.ts
+++ solana/solana-rpc/src/data-source/finalizer.ts
@@ -1,4 +1,4 @@
-import {AsyncQueue, ensureError, last} from '@subsquid/util-internal'
+import {AsyncQueue, ensureError, last, wait} from '@subsquid/util-internal'
 import {BlockBatch, BlockRef, BlockStream} from '@subsquid/util-internal-data-source'
 import {RpcApi} from '../rpc'
 import {Block} from '../types'
@@ -10,6 +10,7 @@ class Finalizer {
     private current?: BlockRef
     private queue: BlockRef[] = []
     private checks = new AsyncQueue<null>(1)
+    private lastRequestTime = 0
 
     constructor(
         private rpc: RpcApi,
@@ -19,13 +20,22 @@ class Finalizer {
     private async finalizationLoop(): Promise<void> {
         for await (let _ of this.checks.iterate()) {
             while (this.queue.length > 0 && !this.output.isClosed()) {
+                await this.probeRateLimit()
                 await this.probe()
             }
         }
     }
 
+    private async probeRateLimit(): Promise<void> {
+        let pause = 500 - Date.now() + this.lastRequestTime
+        if (pause > 0) {
+            await wait(pause)
+        }
+        this.lastRequestTime = Date.now()
+    }
+
     private async probe(): Promise<void> {
-        let probes = this.queue.splice(0, 10)
+        let probes = this.queue.splice(0, 5)
 
         let infos = await this.rpc.getBlockBatch(probes.map(ref => ref.number), {
             commitment: 'finalized',
@@ -44,7 +54,7 @@ class Finalizer {
             if (info == null || info === 'skipped') continue
 
             if (info.blockhash === ref.hash) {
-                this.queue.unshift(...probes.slice(i + 1))
+                this.unshift(probes.slice(i + 1))
                 this.current = ref
                 return this.output.put({
                     blocks: [],
@@ -55,7 +65,16 @@ class Finalizer {
             }
         }
 
-        this.queue.unshift(...probes.slice(i + 1))
+        this.unshift(probes.slice(i + 1))
+    }
+
+    private unshift(probes: BlockRef[]): void {
+        if (probes.length == 0) {
+            // do not apply rate limit, when all probed blocks got finalized
+            this.lastRequestTime = 0
+        } else {
+            this.queue.unshift(...probes)
+        }
     }
 
     private async transformLoop(stream: AsyncIterable<IngestBatch>): Promise<void> {
