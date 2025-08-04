4a5f0c871dfb6b7961862119332badc098e733b1
Damian Figiel <figiel.damian94@gmail.com>
2025-08-04 14:22:38 +0200

    dumper and ingester historical data metrics to open-beta

--- util/util-internal-ingest-cli/src/prometheus.ts
+++ util/util-internal-ingest-cli/src/prometheus.ts
@@ -1,13 +1,16 @@
 import {createPrometheusServer, ListeningServer} from '@subsquid/util-internal-prometheus-server'
-import {collectDefaultMetrics, Counter, Registry} from 'prom-client'
+import {collectDefaultMetrics, Counter, Gauge, Registry} from 'prom-client'
 
 
 export class PrometheusServer {
     private registry = new Registry()
     private s3RequestsCounter: Counter
+    private latestReceivedBlockNumberGauge: Gauge
+    private latestReceivedBlockTimestampGauge: Gauge
+    private blocksDeliveryDelayGauge: Gauge
 
     constructor(
-        private port: number,
+        private port: number
     ) {
         this.s3RequestsCounter = new Counter({
             name: 'sqd_s3_request_count',
@@ -16,11 +19,35 @@ export class PrometheusServer {
             registers: [this.registry],
         })
 
+        this.latestReceivedBlockNumberGauge = new Gauge({
+            name: 'sqd_latest_received_block_number',
+            help: 'Latest block number received',
+            registers: [this.registry]
+        })
+
+        this.latestReceivedBlockTimestampGauge = new Gauge({
+            name: 'sqd_latest_received_block_timestamp',
+            help: 'Timestamp of latest received block',
+            registers: [this.registry]
+        })
+
+        this.blocksDeliveryDelayGauge = new Gauge({
+            name: 'sqd_blocks_delivery_delay',
+            help: 'Delay in seconds between block minted and received',
+            registers: [this.registry]
+        })
+
         collectDefaultMetrics({register: this.registry})
     }
 
     incS3Requests(kind: string, value?: number) {
         this.s3RequestsCounter.inc({kind}, value)
+    }
+
+    setLastReceivedBlock(blockHeight: number, blockTimestamp: number) {
+        this.latestReceivedBlockNumberGauge.set(blockHeight)
+        this.latestReceivedBlockTimestampGauge.set(blockTimestamp)
+        this.blocksDeliveryDelayGauge.set(Math.floor(Date.now() / 1000) - blockTimestamp)
     }
 
     serve(): Promise<ListeningServer> {
