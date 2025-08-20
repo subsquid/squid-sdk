0481c04116b647d613f635c3a0aa954182a511fd
Wizard1209 <34334729+Wizard1209@users.noreply.github.com>
2025-08-20 18:39:12 -0300

    Add RPC ingest to Starknet (#382)
    * Add Starknet RPC package and integrate with existing data ingestion tools
    
    * Add data request interface and implement block mapping and projection functions
    
    * Fix formatting in package.json and add PackedTransaction export in data.ts
    
    * Changelogs for rush
    
    * fix types, tipos and main declaration
    
    * Refactor data types to use option for nullable fields in data.ts
    
    * Fixes according to comments
    
    * Fix validation and ref
    
    * fix response schema bugs
    
    * update packages to upstream master
    
    * reinit pnpm lock
    
    * fix rpc dependency
    
    * fix archive comparasion
    
    ---------
    
    Co-authored-by: Wizard1209 <wizard1209@pm.me>

---
+++ starknet/starknet-rpc/src/rpc.ts
@@ -0,0 +1,130 @@
+import {CallOptions, RpcClient, RpcError, RpcProtocolError} from '@subsquid/rpc-client'
+import {BlockWithTxHashes, Block} from '@subsquid/starknet-data'
+import {Simplify, wait} from '@subsquid/util-internal'
+import {RpcCall, RpcErrorInfo} from '@subsquid/rpc-client/lib/interfaces'
+import {
+    DataValidationError,
+    GetSrcType,
+    nullable,
+    Validator
+} from '@subsquid/util-internal-validation'
+import {RangeRequestList, SplitRequest} from '@subsquid/util-internal-range'
+import {Batch, coldIngest} from '@subsquid/util-internal-ingest-tools'
+import {DataRequest} from './base'
+
+export type BlockHeaderResponse = Simplify<Omit<Block, 'transactions'>>
+
+export interface IngestOptions {
+    stopOnHead?: boolean
+    headPollInterval: number
+    splitSize: number
+    concurrency: number
+};
+
+export class Rpc {
+    constructor(
+        private client: RpcClient,
+        private priority: number = 0,
+    ) {
+    }
+
+    withPriority(priority: number): Rpc {
+        return new Rpc(this.client, priority)
+    }
+
+    call<T=any>(method: string, params?: any[], options?: CallOptions<T>): Promise<T> {
+        return this.client.call(method, params, {priority: this.priority, ...options})
+    }
+
+    batchCall<T=any>(batch: {method: string, params?: any[]}[], options?: CallOptions<T>): Promise<T[]> {
+        return this.client.batchCall(batch, {priority: this.priority, ...options})
+    }
+
+    getFinalizedHeight(): Promise<number> {
+        return this.call('starknet_blockNumber')
+    }
+
+    getBlockHeader(height: number): Promise<BlockHeaderResponse> {
+        return this.call('starknet_getBlockWithTxHashes', [{block_number: height}], {
+            validateResult: getResultValidator(BlockWithTxHashes),
+            validateError: captureBlockNotFound
+        }).then(block => {
+            const typedBlock = block as unknown as Block;
+            const { transactions, ...blockHeader } = typedBlock;
+            return blockHeader as BlockHeaderResponse;
+        });
+    }
+
+    getBlockBatch(heights: number[]): Promise<(Block | null | undefined)[]> {
+        // Prepare the RPC calls
+        let calls: {method: string, params?: any[]}[] = new Array(heights.length)
+        for (let i = 0; i < heights.length; i++) {
+            calls[i] = {
+                method: 'starknet_getBlockWithReceipts',
+                params: [{ block_number: heights[i] }]
+            }
+        }
+
+        return this.reduceBatchOnRetry(calls, {
+            validateResult: getResultValidator(nullable(Block)),
+            validateError: captureBlockNotFound
+        })
+    }
+
+    private async reduceBatchOnRetry<T=any>(batch: {method: string, params?: any[]}[], options: CallOptions<T>): Promise<T[]>  {
+        if (batch.length <= 1) return this.batchCall(batch, options)
+
+        let result = await this.batchCall(batch, {...options, retryAttempts: 0}).catch(err => {
+            if (this.client.isConnectionError(err) || err instanceof RpcProtocolError) return
+            throw err
+        })
+
+        if (result) return result
+
+        let pack = await Promise.all([
+            this.reduceBatchOnRetry(batch.slice(0, Math.ceil(batch.length / 2)), options),
+            this.reduceBatchOnRetry(batch.slice(Math.ceil(batch.length / 2)), options),
+        ])
+
+        return pack.flat()
+    }
+    
+    ingestFinalizedBlocks(requests: RangeRequestList<DataRequest>, options: IngestOptions): AsyncIterable<Batch<Block>> {
+        return coldIngest({
+            getFinalizedHeight: () => this.getFinalizedHeight(),
+            getSplit: (req) => this.getSplitBlocks(req),
+            requests: requests,
+            concurrency: options.concurrency,
+            splitSize: options.splitSize,
+            stopOnHead: options.stopOnHead,
+            headPollInterval: options.headPollInterval
+        });
+    }
+
+    private async getSplitBlocks(req: SplitRequest<DataRequest>): Promise<Block[]> {
+        const heights: number[] = [];
+        for (let height = req.range.from; height <= req.range.to; height++) {
+            heights.push(height);
+        }
+    
+        const blocks = await this.getBlockBatch(heights);
+    
+        return blocks.filter((block): block is Block => block != null);
+    }
+}
+
+export function getResultValidator<V extends Validator>(validator: V): (result: unknown) => GetSrcType<V> {
+    return function(result: unknown) {
+        let err = validator.validate(result)
+        if (err) {
+            throw new DataValidationError(`server returned unexpected result: ${err.toString()}`)
+        } else {
+            return result as any
+        }
+    }
+}
+
+function captureBlockNotFound(info: RpcErrorInfo): undefined {
+    if (/Block not found/i.test(info.message)) return undefined
+    throw new RpcError(info)
+}
--- starknet/starknet-stream/src/index.ts
+++ starknet/starknet-stream/src/index.ts
@@ -1,2 +1,3 @@
 export * from './data/model'
 export * from './source'
+export * from './rpc/client'
