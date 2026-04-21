f28f2dd97db64f93b48f46929c70287cd3eacf90
tmcgroul <safonov.a.r@mail.ru>
2026-04-21 10:55:19 +0300

    remove duplicated prometheus metrics for internal dump package

--- util/util-internal-dump-cli/src/prometheus.ts
+++ util/util-internal-dump-cli/src/prometheus.ts
@@ -42,43 +42,6 @@ export class PrometheusServer {
             }
         })
 
-        this.latestReceivedBlockNumberGauge = new Gauge({
-            name: 'sqd_latest_received_block_number',
-            help: 'Latest block number received',
-            registers: [this.registry]
-        })
-
-        this.latestReceivedBlockTimestampGauge = new Gauge({
-            name: 'sqd_latest_received_block_timestamp',
-            help: 'Timestamp of latest received block',
-            registers: [this.registry]
-        })
-
-        this.blocksDeliveryDelayGauge = new Gauge({
-            name: 'sqd_blocks_delivery_delay',
-            help: 'Delay in seconds between block minted and received',
-            registers: [this.registry]
-        })
-
-        // Duplicate metric of sqd_dump_last_written_block for compatibility with archive.py dumper
-        this.latestProcessedBlockNumberGauge = new Gauge({
-            name: 'sqd_latest_processed_block_number',
-            help: 'Latest processed block number',
-            registers: [this.registry]
-        })
-
-        this.latestProcessedBlockTimestampGauge = new Gauge({
-            name: 'sqd_latest_processed_block_timestamp',
-            help: 'Timestamp of the latest processed block',
-            registers: [this.registry]
-        })
-
-        this.blocksProcessingTimeGauge = new Gauge({
-            name: 'sqd_blocks_processing_time',
-            help: 'Time taken to process blocks (in seconds)',
-            registers: [this.registry]
-        })
-
         this.lastWrittenBlockGauge = new Gauge({
             name: 'sqd_dump_last_written_block',
             help: 'Last saved block',
